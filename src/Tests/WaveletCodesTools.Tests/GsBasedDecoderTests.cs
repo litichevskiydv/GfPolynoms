4dcebd63305dd31860610638fb512de1edb37003
dmitriy.litichevskiy <litichevskiydv@gmail.com>
2016-06-23 23:02:23 +0500

    Draft of list decoder for wavelet codes was added

---
+++ src/Tests/WaveletCodesTools.Tests/GsBasedDecoderTests.cs
@@ -0,0 +1,100 @@
+﻿namespace WaveletCodesTools.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using GfAlgorithms.LinearSystemSolver;
+    using GfPolynoms;
+    using GfPolynoms.Extensions;
+    using GfPolynoms.GaloisFields;
+    using JetBrains.Annotations;
+    using ListDecoderForFixedDistanceCodes;
+    using RsCodesTools.ListDecoder;
+    using RsCodesTools.ListDecoder.GsDecoderDependencies.InterpolationPolynomialBuilder;
+    using RsCodesTools.ListDecoder.GsDecoderDependencies.InterpolationPolynomialFactorisator;
+    using Xunit;
+
+    public class GsBasedDecoderTests
+    {
+        private readonly GsBasedDecoder _decoder;
+
+        [UsedImplicitly]
+        public static readonly IEnumerable<object[]> DecoderTestsData;
+
+        private static Tuple<FieldElement, FieldElement>[] GenerateCodeword(int n, FieldElement generationElement, Polynomial generationPolynomial, Polynomial informationPolynomial)
+        {
+            var field = informationPolynomial.Field;
+
+            var m = new Polynomial(field, 1).RightShift(n);
+            m[0] = field.InverseForAddition(1);
+            var c = (informationPolynomial.RaiseVariableDegree(2) * generationPolynomial) % m;
+
+            var i = 0;
+            var generationElementPower = field.One();
+            var codeword = new Tuple<FieldElement, FieldElement>[n];
+            for (; i <= c.Degree; i++, generationElementPower *= generationElement)
+                codeword[i] = new Tuple<FieldElement, FieldElement>(generationElementPower, new FieldElement(field, c[i]));
+            for (; i < n; i++, generationElementPower *= generationElement)
+                codeword[i] = new Tuple<FieldElement, FieldElement>(generationElementPower, field.Zero());
+
+            return codeword;
+        }
+
+        private static Tuple<FieldElement, FieldElement>[] AddRandomNoise(Tuple<FieldElement, FieldElement>[] codeword, int errorsCount)
+        {
+            var random = new Random();
+            var errorsPositions = new HashSet<int>();
+            while (errorsPositions.Count < errorsCount)
+                errorsPositions.Add(random.Next(codeword.Length));
+
+            var one = codeword[0].Item1.Field.One();
+            foreach (var errorPosition in errorsPositions)
+                codeword[errorPosition].Item2.Add(one);
+
+            return codeword;
+        }
+
+        static GsBasedDecoderTests()
+        {
+            var gf7 = new PrimeOrderField(7);
+            var generationElement = new FieldElement(gf7, 3);
+            var generationPolynomial = new Polynomial(gf7, 4, 2, 6, 4, 3, 4)
+                                        + new Polynomial(gf7, 1, 2, 1, 5, 2, 1).RightShift(2);
+
+            var informationPolynomial1 = new Polynomial(gf7, 4, 0, 2);
+
+            DecoderTestsData = new[]
+                               {
+                                   new object[]
+                                   {
+                                       6, 3, 3, generationPolynomial,
+                                       AddRandomNoise(
+                                           GenerateCodeword(6, generationElement, generationPolynomial, informationPolynomial1), 1),
+                                       5,
+                                       informationPolynomial1
+                                   }
+                               };
+        }
+
+        public GsBasedDecoderTests()
+        {
+            var linearSystemsSolver = new GaussSolver();
+            _decoder = new GsBasedDecoder(new GsDecoder(
+                    new SimplePolynomialBuilder(linearSystemsSolver),
+                    new RrFactorizator()),
+                linearSystemsSolver);
+        }
+
+        [Theory]
+        [MemberData(nameof(DecoderTestsData))]
+        public void ShouldFindOriginalInformationWordAmongPossibleVariants(int n, int k, int d, Polynomial generatingPolynomial,
+            Tuple<FieldElement, FieldElement>[] decodedCodeword, int minCorrectValuesCount, Polynomial expectedInformationPolynomial)
+        {
+            // When
+            var possibleVariants = _decoder.Decode(n, k, d, generatingPolynomial, decodedCodeword, minCorrectValuesCount);
+
+            // Then
+            Assert.True(possibleVariants.Contains(expectedInformationPolynomial));
+        }
+    }
+}
---
+++ src/WaveletCodesTools/ListDecoderForFixedDistanceCodes/GsBasedDecoder.cs
@@ -0,0 +1,96 @@
+﻿namespace WaveletCodesTools.ListDecoderForFixedDistanceCodes
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using GfAlgorithms.LinearSystemSolver;
+    using GfPolynoms;
+    using GfPolynoms.Extensions;
+
+    public class GsBasedDecoder : IListDecoder
+    {
+        private readonly RsCodesTools.ListDecoder.IListDecoder _rsListDecoder;
+        private readonly ILinearSystemSolver _linearSystemSolver;
+
+        private IEnumerable<Polynomial> GetFrequencyDecodingResults(int n, int d,
+            IReadOnlyList<Tuple<FieldElement, FieldElement>> decodedCodeword, int minCorrectValuesCount)
+        {
+            var field = decodedCodeword[0].Item1.Field;
+
+            var correction = new FieldElement(field, n);
+            var preparedCodeword = new Tuple<FieldElement, FieldElement>[n];
+            for (var i = 0; i < n; i++)
+            {
+                var inversedSample = new FieldElement(field, field.InverseForMultiplication(decodedCodeword[i].Item1.Representation));
+                preparedCodeword[i] = new Tuple<FieldElement, FieldElement>(inversedSample,
+                    decodedCodeword[i].Item2*correction*FieldElement.Pow(decodedCodeword[i].Item1, d - 1));
+            }
+            return _rsListDecoder.Decode(n, n - d + 1, preparedCodeword, minCorrectValuesCount);
+        }
+
+        private Polynomial[] SelectCorrectInformationPolynomials(int n, int k, int d, Polynomial generatingPolynomial,
+            IReadOnlyList<Tuple<FieldElement, FieldElement>> decodedCodeword, IEnumerable<Polynomial> frequencyDecodingResults)
+        {
+            var correctPolynomials = new List<Polynomial>();
+
+            var field = generatingPolynomial.Field;
+            foreach (var frequencyDecodingResult in frequencyDecodingResults)
+            {
+                var linearSystemMatrix = new FieldElement[n, k];
+                for (var i = 0; i < n; i++)
+                {
+                    var correction = new FieldElement(field, generatingPolynomial.Evaluate(decodedCodeword[i].Item1.Representation));
+                    var samplePower = field.One();
+                    var sampleSqr = decodedCodeword[i].Item1*decodedCodeword[i].Item1;
+                    for (var j = 0; j < k; j++)
+                    {
+                        linearSystemMatrix[i, j] = samplePower*correction;
+                        samplePower *= sampleSqr;
+                    }
+                }
+
+                var valuesVector = new FieldElement[n];
+                for (var i = 0; i < n; i++)
+                    valuesVector[i] = field.Zero();
+                for (int i = d - 1, j = 0; i < n && j <= frequencyDecodingResult.Degree; i++,j++)
+                    valuesVector[i] = new FieldElement(field, frequencyDecodingResult[j]);
+
+                var systemSolution = _linearSystemSolver.Solve(linearSystemMatrix, valuesVector);
+                if (systemSolution.IsEmpty == false)
+                    correctPolynomials.Add(new Polynomial(field, systemSolution.Solution.Select(x => x.Representation).ToArray()));
+            }
+
+            return correctPolynomials.ToArray();
+        }
+
+        public Polynomial[] Decode(int n, int k, int d, Polynomial generatingPolynomial, 
+            Tuple<FieldElement, FieldElement>[] decodedCodeword, int minCorrectValuesCount)
+        {
+            if (n <= 0)
+                throw new ArgumentException(nameof(n));
+            if (k <= 0 || k >= n)
+                throw new ArithmeticException(nameof(k));
+            if(generatingPolynomial == null)
+                throw new ArgumentNullException(nameof(generatingPolynomial));
+            if (decodedCodeword == null)
+                throw new ArgumentNullException(nameof(decodedCodeword));
+            if (decodedCodeword.Length != n)
+                throw new AggregateException(nameof(decodedCodeword));
+
+            var frequencyDecodingResults = GetFrequencyDecodingResults(n, d, decodedCodeword, minCorrectValuesCount);
+
+            return SelectCorrectInformationPolynomials(n, k, d, generatingPolynomial, decodedCodeword, frequencyDecodingResults);
+        }
+
+        public GsBasedDecoder(RsCodesTools.ListDecoder.IListDecoder rsListDecoder, ILinearSystemSolver linearSystemSolver)
+        {
+            if (rsListDecoder == null)
+                throw new ArgumentNullException(nameof(rsListDecoder));
+            if (linearSystemSolver == null)
+                throw new ArgumentNullException(nameof(linearSystemSolver));
+
+            _rsListDecoder = rsListDecoder;
+            _linearSystemSolver = linearSystemSolver;
+        }
+    }
+}
