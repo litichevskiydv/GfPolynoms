5c1f60a873304480acfb4758ddf943ff91e3e5e4
dmitriy.litichevskiy <litichevskiydv@gmail.com>
2016-10-01 01:57:12 +0500

    Framework for tests was specified

--- build/RunTests.bat
+++ build/RunTests.bat
@@ -1,4 +1,4 @@
-dotnet test "test\GfPolynoms.Tests" -c %CONFIGURATION%
-dotnet test "test\GfAlgorithms.Tests" -c %CONFIGURATION%
-dotnet test "test\RsCodesTools.Tests" -c %CONFIGURATION%
-dotnet test "test\WaveletCodesTools.Tests" -c %CONFIGURATION%
+dotnet test "test\GfPolynoms.Tests" -c %CONFIGURATION% -f netcoreapp1.0
+dotnet test "test\GfAlgorithms.Tests" -c %CONFIGURATION% -f netcoreapp1.0
+dotnet test "test\RsCodesTools.Tests" -c %CONFIGURATION% -f netcoreapp1.0
+dotnet test "test\WaveletCodesTools.Tests" -c %CONFIGURATION% -f netcoreapp1.0
