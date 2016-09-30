249e554d10aa54b721e97140e2ff4cb2ae8478b4
dmitriy.litichevskiy <litichevskiydv@gmail.com>
2016-10-01 02:22:35 +0500

    Another projects were added to script

--- build/SetFullDebugType.ps1
+++ build/SetFullDebugType.ps1
@@ -1,1 +1,4 @@
 (Get-Content src\GfPolynoms\project.json) -replace 'portable', 'full' | Set-Content src\GfPolynoms\project.json
+(Get-Content src\GfAlgorithms\project.json) -replace 'portable', 'full' | Set-Content src\GfAlgorithms\project.json
+(Get-Content src\RsCodesTools\project.json) -replace 'portable', 'full' | Set-Content src\RsCodesTools\project.json
+(Get-Content src\WaveletCodesTools\project.json) -replace 'portable', 'full' | Set-Content src\WaveletCodesTools\project.json
