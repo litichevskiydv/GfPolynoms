480354e228a66ac03c16ec75127f79cc1f1b70d2
dmitriy.litichevskiy <litichevskiydv@gmail.com>
2016-10-01 01:26:12 +0500

    Script for tests running was added

---
+++ build/RunTests.bat
@@ -0,0 +1,4 @@
+dotnet test "test\GfPolynoms.Tests" -c %CONFIGURATION%
+dotnet test "test\GfAlgorithms.Tests" -c %CONFIGURATION%
+dotnet test "test\RsCodesTools.Tests" -c %CONFIGURATION%
+dotnet test "test\WaveletCodesTools.Tests" -c %CONFIGURATION%
