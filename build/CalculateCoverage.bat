10d23e2495ef3be9171d4fea0e50cbff591b9f17
dmitriy.litichevskiy <litichevskiydv@gmail.com>
2016-10-03 23:08:22 +0500

    Tests coverage calculation was moved to separate bat file

---
+++ build/CalculateCoverage.bat
@@ -0,0 +1,9 @@
+nuget install OpenCover -ExcludeVersion -OutputDirectory tools
+powershell.exe -file build\SetFullDebugType.ps1
+tools\OpenCover\tools\OpenCover.Console.exe -target:"C:\Program Files\dotnet\dotnet.exe" -targetargs:" test ""test\GfPolynoms.Tests\project.json"" -c Debug -f net46" -register:user -filter:"+[*]* -[xunit*]*" -threshold:100 -oldstyle -returntargetcode -output:coverage.xml
+tools\OpenCover\tools\OpenCover.Console.exe -target:"C:\Program Files\dotnet\dotnet.exe" -targetargs:" test ""test\GfAlgorithms.Tests\project.json"" -c Debug -f net46" -register:user -filter:"+[*]* -[xunit*]*" -threshold:100 -oldstyle -returntargetcode -mergeoutput -output:coverage.xml
+tools\OpenCover\tools\OpenCover.Console.exe -target:"C:\Program Files\dotnet\dotnet.exe" -targetargs:" test ""test\RsCodesTools.Tests\project.json"" -c Debug -f net46" -register:user -filter:"+[*]* -[xunit*]*" -threshold:100 -oldstyle -returntargetcode -mergeoutput -output:coverage.xml
+tools\OpenCover\tools\OpenCover.Console.exe -target:"C:\Program Files\dotnet\dotnet.exe" -targetargs:" test ""test\WaveletCodesTools.Tests\project.json"" -c Debug -f net46" -register:user -filter:"+[*]* -[xunit*]*" -threshold:100 -oldstyle -returntargetcode -mergeoutput -output:coverage.xml
+SET PATH=C:\\Python34;C:\\Python34\\Scripts;%PATH%
+pip install codecov
+codecov -f "coverage.xml"
