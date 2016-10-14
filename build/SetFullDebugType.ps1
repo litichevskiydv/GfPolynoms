37c70eac79a0e707012ff79cd96e312f88be22cd
dmitriy.litichevskiy <litichevskiydv@gmail.com>
2016-10-14 23:35:23 +0500

    Fexes for build were added

--- build/SetFullDebugType.ps1
+++ build/SetFullDebugType.ps1
@@ -1,4 +1,4 @@
-(Get-Content src\GfPolynoms\project.json) -replace 'portable', 'full' | Set-Content src\GfPolynoms\project.json
-(Get-Content src\GfAlgorithms\project.json) -replace 'portable', 'full' | Set-Content src\GfAlgorithms\project.json
-(Get-Content src\RsCodesTools\project.json) -replace 'portable', 'full' | Set-Content src\RsCodesTools\project.json
-(Get-Content src\WaveletCodesTools\project.json) -replace 'portable', 'full' | Set-Content src\WaveletCodesTools\project.json
+(Get-Content src\AppliedAlgebra.GfPolynoms\project.json) -replace 'portable', 'full' | Set-Content src\GfPolynoms\project.json
+(Get-Content src\AppliedAlgebra.GfAlgorithms\project.json) -replace 'portable', 'full' | Set-Content src\GfAlgorithms\project.json
+(Get-Content src\AppliedAlgebra.RsCodesTools\project.json) -replace 'portable', 'full' | Set-Content src\RsCodesTools\project.json
+(Get-Content src\AppliedAlgebra.WaveletCodesTools\project.json) -replace 'portable', 'full' | Set-Content src\WaveletCodesTools\project.json
