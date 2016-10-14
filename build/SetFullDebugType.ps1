9de019970a82778d30a456852885e17432e0fda5
dmitriy.litichevskiy <litichevskiydv@gmail.com>
2016-10-15 00:24:48 +0500

    Fixes for directories

--- build/SetFullDebugType.ps1
+++ build/SetFullDebugType.ps1
@@ -1,4 +1,4 @@
-(Get-Content src\AppliedAlgebra.GfPolynoms\project.json) -replace 'portable', 'full' | Set-Content src\GfPolynoms\project.json
-(Get-Content src\AppliedAlgebra.GfAlgorithms\project.json) -replace 'portable', 'full' | Set-Content src\GfAlgorithms\project.json
-(Get-Content src\AppliedAlgebra.RsCodesTools\project.json) -replace 'portable', 'full' | Set-Content src\RsCodesTools\project.json
-(Get-Content src\AppliedAlgebra.WaveletCodesTools\project.json) -replace 'portable', 'full' | Set-Content src\WaveletCodesTools\project.json
+(Get-Content src\AppliedAlgebra.GfPolynoms\project.json) -replace 'portable', 'full' | Set-Content src\AppliedAlgebra.GfPolynoms\project.json
+(Get-Content src\AppliedAlgebra.GfAlgorithms\project.json) -replace 'portable', 'full' | Set-Content src\AppliedAlgebra.GfAlgorithms\project.json
+(Get-Content src\AppliedAlgebra.RsCodesTools\project.json) -replace 'portable', 'full' | Set-Content src\AppliedAlgebra.RsCodesTools\project.json
+(Get-Content src\AppliedAlgebra.WaveletCodesTools\project.json) -replace 'portable', 'full' | Set-Content src\AppliedAlgebra.WaveletCodesTools\project.json
