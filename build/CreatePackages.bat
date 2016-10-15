aaa43ab8e44699689950f017003a5dc44113c3b9
dmitriy.litichevskiy <litichevskiydv@gmail.com>
2016-10-15 19:30:50 +0500

    Build configuration was updated

--- build/CreatePackages.bat
+++ build/CreatePackages.bat
@@ -1,4 +1,4 @@
-dotnet pack "src\GfPolynoms" -c %CONFIGURATION% --version-suffix %APPVEYOR_BUILD_NUMBER%
-dotnet pack "src\GfAlgorithms" -c %CONFIGURATION% --version-suffix %APPVEYOR_BUILD_NUMBER%
-dotnet pack "src\RsCodesTools" -c %CONFIGURATION% --version-suffix %APPVEYOR_BUILD_NUMBER%
-dotnet pack "src\WaveletCodesTools" -c %CONFIGURATION% --version-suffix %APPVEYOR_BUILD_NUMBER%
+dotnet pack "src\GfPolynoms" -c %CONFIGURATION% --version-suffix beta%APPVEYOR_BUILD_NUMBER%
+dotnet pack "src\GfAlgorithms" -c %CONFIGURATION% --version-suffix beta%APPVEYOR_BUILD_NUMBER%
+dotnet pack "src\RsCodesTools" -c %CONFIGURATION% --version-suffix beta%APPVEYOR_BUILD_NUMBER%
+dotnet pack "src\WaveletCodesTools" -c %CONFIGURATION% --version-suffix beta%APPVEYOR_BUILD_NUMBER%
