70472df0662b6019ce055da343818dfc060d4b31
dmitriy.litichevskiy <litichevskiydv@gmail.com>
2017-01-12 20:35:33 +0500

    Checks for test runner results were added

--- build/CalculateCoverage.bat
+++ build/CalculateCoverage.bat
@@ -1,6 +1,6 @@
 powershell.exe -file build\SetFullDebugType.ps1
-
 nuget install OpenCover -ExcludeVersion -OutputDirectory tools
+
 tools\OpenCover\tools\OpenCover.Console.exe -target:"C:\Program Files\dotnet\dotnet.exe" -targetargs:" test ""test\GfPolynoms.Tests\project.json"" -c Debug" -register:user -filter:"+[*]* -[xunit*]*" -threshold:100 -oldstyle -returntargetcode -output:coverage.xml
 if %errorlevel% neq 0 exit /b %errorlevel%
 tools\OpenCover\tools\OpenCover.Console.exe -target:"C:\Program Files\dotnet\dotnet.exe" -targetargs:" test ""test\GfAlgorithms.Tests\project.json"" -c Debug" -register:user -filter:"+[*]* -[xunit*]*" -threshold:100 -oldstyle -returntargetcode -mergeoutput -output:coverage.xml
--- build/RunTests.bat
+++ build/RunTests.bat
@@ -1,4 +1,8 @@
 dotnet test "test\GfPolynoms.Tests" -c %1
+if %errorlevel% neq 0 exit /b %errorlevel%
 dotnet test "test\GfAlgorithms.Tests" -c %1
+if %errorlevel% neq 0 exit /b %errorlevel%
 dotnet test "test\RsCodesTools.Tests" -c %1
+if %errorlevel% neq 0 exit /b %errorlevel%
 dotnet test "test\WaveletCodesTools.Tests" -c %1
+if %errorlevel% neq 0 exit /b %errorlevel%
