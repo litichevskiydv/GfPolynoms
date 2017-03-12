f6163ccc284c4562053d831084921542ddede364
dmitriy.litichevskiy <litichevskiydv@gmail.com>
2017-03-12 12:53:45 +0500

    Build configuaration was updated

--- build/CreatePackages.bat
+++ build/CreatePackages.bat
@@ -1,4 +1,4 @@
-dotnet pack "src\GfPolynoms" -c %1 --version-suffix %2
-dotnet pack "src\GfAlgorithms" -c %1 --version-suffix %2
-dotnet pack "src\RsCodesTools" -c %1 --version-suffix %2
-dotnet pack "src\WaveletCodesTools" -c %1 --version-suffix %2
+dotnet pack "src\GfPolynoms" -c %1 --include-symbols --version-suffix %2
+dotnet pack "src\GfAlgorithms" -c %1 --include-symbols --version-suffix %2
+dotnet pack "src\RsCodesTools" -c %1 --include-symbols --version-suffix %2
+dotnet pack "src\WaveletCodesTools" -c %1 --include-symbols --version-suffix %2
