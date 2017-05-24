37e60040fd557b08d02b08b5a61538b4df94d30f
dmitriy.litichevskiy <litichevskiydv@gmail.com>
2017-05-24 23:12:58 +0500

    Comments were added in wavelet code's encoder

--- src/WaveletCodesTools/Encoder/Encoder.cs
+++ src/WaveletCodesTools/Encoder/Encoder.cs
@@ -4,8 +4,19 @@
     using GfPolynoms;
     using GfPolynoms.Extensions;
 
+    /// <summary>
+    /// Implementation of the wavelet code's encoder contract
+    /// </summary>
     public class Encoder : IEncoder
     {
+        /// <summary>
+        /// Method for computing wavelet code's codeword for information polynomial <paramref name="informationPolynomial"/>
+        /// </summary>
+        /// <param name="n">Codeword length</param>
+        /// <param name="generatingPolynomial">Generating polynomial of the wavelet code</param>
+        /// <param name="informationPolynomial">Information polynomial</param>
+        /// <param name="modularPolynomial">Modular polynomial for wavelet code encoding scheme</param>
+        /// <returns>Computed codeword</returns>
         public Tuple<FieldElement, FieldElement>[] Encode(int n, Polynomial generatingPolynomial, Polynomial informationPolynomial,
             Polynomial modularPolynomial = null)
         {
--- src/WaveletCodesTools/Encoder/IEncoder.cs
+++ src/WaveletCodesTools/Encoder/IEncoder.cs
@@ -3,8 +3,19 @@
     using System;
     using GfPolynoms;
 
+    /// <summary>
+    /// Contract for the wavelet code encoder
+    /// </summary>
     public interface IEncoder
     {
+        /// <summary>
+        /// Method for computing wavelet code's codeword for information polynomial <paramref name="informationPolynomial"/>
+        /// </summary>
+        /// <param name="n">Codeword length</param>
+        /// <param name="generatingPolynomial">Generating polynomial of the wavelet code</param>
+        /// <param name="informationPolynomial">Information polynomial</param>
+        /// <param name="modularPolynomial">Modular polynomial for wavelet code encoding scheme</param>
+        /// <returns>Computed codeword</returns>
         Tuple<FieldElement, FieldElement>[] Encode(int n, Polynomial generatingPolynomial, Polynomial informationPolynomial,
             Polynomial modularPolynomial = null);
     }
