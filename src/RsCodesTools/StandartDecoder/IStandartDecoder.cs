1dad768b8073a55d6820ce53d15295e5e2dd7ab4
dmitriy.litichevskiy <litichevskiydv@gmail.com>
2017-06-04 18:52:50 +0500

    Contract for Reed-Solomon code decoder was added

---
+++ src/RsCodesTools/StandartDecoder/IStandartDecoder.cs
@@ -0,0 +1,21 @@
+﻿namespace AppliedAlgebra.RsCodesTools.StandartDecoder
+{
+    using System;
+    using GfPolynoms;
+
+    /// <summary>
+    /// Contract for Reed-Solomon code decoder
+    /// </summary>
+    public interface IStandartDecoder
+    {
+        /// <summary>
+        /// Method for performing decoding of Reed-Solomon code codeword
+        /// </summary>
+        /// <param name="n">Codeword length</param>
+        /// <param name="k">Information word length</param>
+        /// <param name="decodedCodeword">Recived codeword for decoding</param>
+        /// <param name="errorsCount">Errors count</param>
+        /// <returns>Decoding result</returns>
+        Polynomial Decode(int n, int k, Tuple<FieldElement, FieldElement>[] decodedCodeword, int errorsCount);
+    }
+}
---
+++ src/RsCodesTools/StandartDecoder/InformationWordWasNotFoundException.cs
@@ -0,0 +1,11 @@
+﻿namespace AppliedAlgebra.RsCodesTools.StandartDecoder
+{
+    using System;
+
+    /// <summary>
+    /// Exception for indication that information polynomial cannot be found
+    /// </summary>
+    public class InformationWordWasNotFoundException : InvalidOperationException
+    {
+    }
+}
