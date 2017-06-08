7b12b3e29d2ae021b8a2fd027a85dcd5b6aefcd5
dmitriy.litichevskiy <litichevskiydv@gmail.com>
2017-06-08 09:01:31 +0500

    Contract for Reed-Solomon code's encoder was added

---
+++ src/RsCodesTools/Encoder/IEncoder.cs
@@ -0,0 +1,19 @@
+﻿namespace AppliedAlgebra.RsCodesTools.Encoder
+{
+    using System;
+    using GfPolynoms;
+
+    /// <summary>
+    /// Contract for the Reed-Solomon code encoder
+    /// </summary>
+    public interface IEncoder
+    {
+        /// <summary>
+        /// Method for computing Reed-Solomon code's codeword for information polynomial <paramref name="informationPolynomial"/>
+        /// </summary>
+        /// <param name="n">Codeword length</param>
+        /// <param name="informationPolynomial">Information polynomial</param>
+        /// <returns>Computed codeword</returns>
+        Tuple<FieldElement, FieldElement>[] Encode(int n, Polynomial informationPolynomial);
+    }
+}
