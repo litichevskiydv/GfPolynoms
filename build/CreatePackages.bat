bbdd16e8689a5a299785d381ed422ea7c447ff11
dmitriy.litichevskiy <litichevskiydv@gmail.com>
2017-08-06 10:17:40 +0500

    Cake build scripts were added

--- build/CreatePackages.bat
+++
@@ -1,4 +0,0 @@
-dotnet pack "src\GfPolynoms" -c %1 --include-symbols --version-suffix %2
-dotnet pack "src\GfAlgorithms" -c %1 --include-symbols --version-suffix %2
-dotnet pack "src\RsCodesTools" -c %1 --include-symbols --version-suffix %2
-dotnet pack "src\WaveletCodesTools" -c %1 --include-symbols --version-suffix %2
--- build/SetFullDebugType.ps1
+++
@@ -1,4 +0,0 @@
-(Get-Content src\GfPolynoms\GfPolynoms.csproj) -replace 'portable', 'full' | Set-Content src\GfPolynoms\GfPolynoms.csproj
-(Get-Content src\GfAlgorithms\GfAlgorithms.csproj) -replace 'portable', 'full' | Set-Content src\GfAlgorithms\GfAlgorithms.csproj
-(Get-Content src\RsCodesTools\RsCodesTools.csproj) -replace 'portable', 'full' | Set-Content src\RsCodesTools\RsCodesTools.csproj
-(Get-Content src\WaveletCodesTools\WaveletCodesTools.csproj) -replace 'portable', 'full' | Set-Content src\WaveletCodesTools\WaveletCodesTools.csproj
