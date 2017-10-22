b019994c7293aef2043ba9b5947a020f64c41830
dmitriy.litichevskiy <litichevskiydv@gmail.com>
2017-10-22 10:25:01 +0500

    Travis CI build configuration was updated

--- build/build.ps1
+++ build/build.ps1
@@ -5,7 +5,7 @@ Param(
     [ValidateSet("Release", "Debug")]
     [string]$Configuration = "Release",
     [ValidateSet("Quiet", "Minimal", "Normal", "Verbose", "Diagnostic")]
-    [string]$Verbosity = "Verbose",
+    [string]$Verbosity = "Diagnostic",
     [Alias("DryRun","Noop")]
     [switch]$WhatIf,
     [Parameter(Position=0,Mandatory=$false,ValueFromRemainingArguments=$true)]
