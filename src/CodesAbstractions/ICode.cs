6f371cd4bff1d0de9d2e63c09f16761958384d1c
dmitriy.litichevskiy <litichevskiydv@gmail.com>
2018-12-13 22:53:52 +0500

    Common contract for codes implementations was added

---
+++ src/CodesAbstractions/ICode.cs
@@ -0,0 +1,21 @@
+﻿namespace AppliedAlgebra.CodesAbstractions
+{
+    using System.Collections.Generic;
+    using GfPolynoms;
+
+    /// <summary>
+    /// Contract for code implementation
+    /// </summary>
+    public interface ICode
+    {
+        int CodewordLength { get; }
+        int InformationWordLength { get; }
+        int CodeDistance { get; }
+
+        FieldElement[] Encode(FieldElement[] informationWord);
+
+        FieldElement[] Decode(FieldElement[] codeword);
+
+        IReadOnlyCollection<FieldElement[]> DecodeViaList(FieldElement[] codeword);
+    }
+}
