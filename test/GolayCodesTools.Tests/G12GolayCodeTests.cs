afaeabb6071c57880873d3849117fc33d12b58b4
dmitriy.litichevskiy <litichevskiydv@gmail.com>
2018-12-21 18:47:16 +0500

    Tests for G12 Golay code were added

---
+++ test/GolayCodesTools.Tests/G12GolayCodeTests.cs
@@ -0,0 +1,82 @@
+﻿namespace AppliedAlgebra.GolayCodesTools.Tests
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using CodesResearchTools.NoiseGenerator;
+    using GfAlgorithms.Extensions;
+    using GfPolynoms;
+    using GfPolynoms.Extensions;
+    using GfPolynoms.GaloisFields;
+    using JetBrains.Annotations;
+    using Xunit;
+
+    public class G12GolayCodeTests
+    {
+        [UsedImplicitly] public static readonly IEnumerable<object[]> DecodeTestsData;
+
+        private readonly G12GolayCode _code;
+        private readonly INoiseGenerator _noiseGenerator;
+
+        static G12GolayCodeTests()
+        {
+            var gf3 = new PrimeOrderField(3);
+            FieldElement[] InformationWordBuilder(IEnumerable<int> array) => array.Select(x => gf3.CreateElement(x)).ToArray();
+
+            DecodeTestsData
+                = new[]
+                  {
+                      new object[] {InformationWordBuilder(new[] {1, 2, 0, 0, 1, 2})},
+                      new object[] {InformationWordBuilder(new[] {0, 1, 2, 1, 2, 2})},
+                      new object[] {InformationWordBuilder(new[] {0, 0, 0, 2, 1, 1})},
+                      new object[] {InformationWordBuilder(new[] {0, 0, 0, 0, 0, 0})},
+                      new object[] {InformationWordBuilder(new[] {2, 2, 2, 2, 2, 2})},
+                  };
+        }
+
+        public G12GolayCodeTests()
+        {
+            _code = new G12GolayCode();
+            _noiseGenerator = new RecursiveGenerator();
+        }
+
+        [Theory]
+        [MemberData(nameof(DecodeTestsData))]
+        public void ShouldTestDecodeMethod(FieldElement[] expectedInformationWord)
+        {
+            // Given
+            var codeword = _code.Encode(expectedInformationWord);
+            var noisyCodeword =
+                codeword.AddNoise(
+                    _noiseGenerator.VariatePositionsAndValues(_code.Field, _code.CodewordLength, (_code.CodeDistance - 1) / 2)
+                        .Skip(50)
+                        .First()
+                );
+
+            // When
+            var actualInformationWord = _code.Decode(noisyCodeword);
+
+            // Then
+            Assert.Equal(expectedInformationWord, actualInformationWord);
+        }
+
+        [Theory]
+        [MemberData(nameof(DecodeTestsData))]
+        public void ShouldTestDecodeViaListMethod(FieldElement[] expectedInformationWord)
+        {
+            // Given
+            var codeword = _code.Encode(expectedInformationWord);
+            var noisyCodeword =
+                codeword.AddNoise(
+                    _noiseGenerator.VariatePositionsAndValues(_code.Field, _code.CodewordLength, (_code.CodeDistance - 1) / 2 + 1)
+                        .Skip(100)
+                        .First()
+                );
+
+            // When
+            var actualInformationWords = _code.DecodeViaList(noisyCodeword);
+
+            // Then
+            Assert.Contains(expectedInformationWord, actualInformationWords);
+        }
+    }
+}
