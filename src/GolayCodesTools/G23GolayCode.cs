bb43b36e8ca88b0778edd68fd84dc306943220c4
dmitriy.litichevskiy <litichevskiydv@gmail.com>
2019-01-05 18:55:21 +0500

    Generating polynomial was updated for G23 and G24

--- src/GolayCodesTools/G23GolayCode.cs
+++ src/GolayCodesTools/G23GolayCode.cs
@@ -5,12 +5,7 @@
 
     public class G23GolayCode : GolayCodeBase
     {
-        public G23GolayCode() : base(
-            23,
-            12,
-            7,
-            new Polynomial(new PrimeOrderField(2), 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1)
-        )
+        public G23GolayCode() : base(23, 12, 7, new Polynomial(new PrimeOrderField(2), 1, 0, 1, 0, 1, 1, 1, 0, 0, 0, 1, 1))
         {
         }
     }
