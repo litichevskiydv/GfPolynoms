c72a65b9493805e9b03ea3020410cb186729eddf
dmitriy.litichevskiy <litichevskiydv@gmail.com>
2019-05-28 21:06:04 +0500

    TheoryData was used in bivariate monomials comparer tests

--- test/GfAlgorithms.Tests/BiVariableMonomialsComparerTests.cs
+++ test/GfAlgorithms.Tests/BiVariableMonomialsComparerTests.cs
@@ -1,40 +1,64 @@
 ﻿namespace AppliedAlgebra.GfAlgorithms.Tests
 {
     using System;
-    using System.Collections.Generic;
     using BiVariablePolynomials;
     using JetBrains.Annotations;
     using Xunit;
 
     public class BiVariableMonomialsComparerTests
     {
+        public class MonomialsComparsionTestCase
+        {
+            public Tuple<int, int> FirstOperand { get; set; }
+
+            public Tuple<int, int> SecondOperand { get; set; }
+
+            public int ExpectedResult { get; set; }
+        }
+
         private readonly BiVariableMonomialsComparer _comparer;
 
         [UsedImplicitly]
-        public static readonly IEnumerable<object[]> ComparsionTestsData;
+        public static readonly TheoryData<MonomialsComparsionTestCase> ComparsionTestsData;
 
         static BiVariableMonomialsComparerTests()
         {
-            ComparsionTestsData = new[]
-                                  {
-                                      new object[] {new Tuple<int, int>(1, 1), new Tuple<int, int>(1, 1), 0},
-                                      new object[] {new Tuple<int, int>(0, 1), new Tuple<int, int>(1, 1), -1},
-                                      new object[] {new Tuple<int, int>(1, 1), new Tuple<int, int>(0, 1), 1},
-                                      new object[] {new Tuple<int, int>(0, 2), new Tuple<int, int>(3, 1), -1},
-                                      new object[] {new Tuple<int, int>(3, 1), new Tuple<int, int>(0, 2), 1}
-                                  };
+            ComparsionTestsData
+                = new TheoryData<MonomialsComparsionTestCase>
+                  {
+                      new MonomialsComparsionTestCase
+                      {
+                          FirstOperand = Tuple.Create(1, 1), SecondOperand = Tuple.Create(1, 1), ExpectedResult = 0
+                      },
+                      new MonomialsComparsionTestCase
+                      {
+                          FirstOperand = Tuple.Create(0, 1), SecondOperand = Tuple.Create(1, 1), ExpectedResult = -1
+                      },
+                      new MonomialsComparsionTestCase
+                      {
+                          FirstOperand = Tuple.Create(1, 1), SecondOperand = Tuple.Create(0, 1), ExpectedResult = 1
+                      },
+                      new MonomialsComparsionTestCase
+                      {
+                          FirstOperand = Tuple.Create(0, 2), SecondOperand = Tuple.Create(3, 1), ExpectedResult = -1
+                      },
+                      new MonomialsComparsionTestCase
+                      {
+                          FirstOperand = Tuple.Create(3, 1), SecondOperand = Tuple.Create(0, 2), ExpectedResult = 1
+                      }
+                  };
         }
 
         public BiVariableMonomialsComparerTests()
         {
-            _comparer = new BiVariableMonomialsComparer(new Tuple<int, int>(1, 3));
+            _comparer = new BiVariableMonomialsComparer(Tuple.Create(1, 3));
         }
 
         [Theory]
         [MemberData(nameof(ComparsionTestsData))]
-        public void ShoulCompareTwoMonomials(Tuple<int, int> a, Tuple<int, int> b, int expectedComparsionResult)
+        public void ShoulCompareTwoMonomials(MonomialsComparsionTestCase testCase)
         {
-            Assert.Equal(Math.Sign(expectedComparsionResult), Math.Sign(_comparer.Compare(a, b)));
+            Assert.Equal(Math.Sign(testCase.ExpectedResult), Math.Sign(_comparer.Compare(testCase.FirstOperand, testCase.SecondOperand)));
         }
     }
 }
