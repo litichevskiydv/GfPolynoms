40b1e0465d62e572fe2bea227272dd1a3b3c57c2
dmitriy.litichevskiy <litichevskiydv@gmail.com>
2019-06-28 00:49:22 +0500

    TheoryData was used in complementary polynomial building tests

--- test/GfAlgorithms.Tests/GcdBasedBuilderTests.cs
+++ test/GfAlgorithms.Tests/GcdBasedBuilderTests.cs
@@ -10,10 +10,19 @@
 
     public class GcdBasedBuilderTests
     {
+        public class ComplementaryPolynomialBuildingTestCase
+        {
+            public Polynomial SourceFilter { get; set; }
+
+            public int MaxFilterLength { get; set; }
+
+            public Polynomial Expected { get; set; }
+        }
+
         private readonly GcdBasedBuilder _builder;
 
         [UsedImplicitly]
-        public static readonly IEnumerable<object[]> BuildTestsData;
+        public static readonly TheoryData<ComplementaryPolynomialBuildingTestCase> BuildTestsData;
 
         static GcdBasedBuilderTests()
         {
@@ -22,34 +31,40 @@
             var gf11 = new PrimeOrderField(11);
             var gf17 = new PrimeOrderField(17);
 
-            BuildTestsData = new[]
-                             {
-                                 new object[]
-                                 {
-                                     new Polynomial(gf2, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1), 24,
-                                     new Polynomial(gf2, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1)
-                                 },
-                                 new object[]
-                                 {
-                                     new Polynomial(gf7, 3, 2, 5, 0, 4), 6,
-                                     new Polynomial(gf7, 3)
-                                 },
-                                 new object[]
-                                 {
-                                     new Polynomial(gf7, 4, 2, 6, 4, 3, 4), 6,
-                                     new Polynomial(gf7, 0, 2, 2, 5)
-                                 },
-                                 new object[]
-                                 {
-                                     new Polynomial(gf11, 0, 0, 0, 0, 0, 10, 5, 4, 3, 4), 10,
-                                     new Polynomial(gf11, 0, 0, 0, 0, 0, 0, 1, 1, 10, 6)
-                                 },
-                                 new object[]
-                                 {
-                                     new Polynomial(gf17, 10, 16, 5, 0, 0, 0, 0, 16), 16,
-                                     new Polynomial(gf17, 12, 4, 0, 15, 0, 1)
-                                 }
-                             };
+            BuildTestsData
+                = new TheoryData<ComplementaryPolynomialBuildingTestCase>
+                  {
+                      new ComplementaryPolynomialBuildingTestCase
+                      {
+                          SourceFilter = new Polynomial(gf2, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1),
+                          MaxFilterLength = 24,
+                          Expected = new Polynomial(gf2, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1)
+                      },
+                      new ComplementaryPolynomialBuildingTestCase
+                      {
+                          SourceFilter = new Polynomial(gf7, 3, 2, 5, 0, 4),
+                          MaxFilterLength = 6,
+                          Expected = new Polynomial(gf7, 3)
+                      },
+                      new ComplementaryPolynomialBuildingTestCase
+                      {
+                          SourceFilter = new Polynomial(gf7, 4, 2, 6, 4, 3, 4),
+                          MaxFilterLength = 6,
+                          Expected = new Polynomial(gf7, 0, 2, 2, 5)
+                      },
+                      new ComplementaryPolynomialBuildingTestCase
+                      {
+                          SourceFilter = new Polynomial(gf11, 0, 0, 0, 0, 0, 10, 5, 4, 3, 4),
+                          MaxFilterLength = 10,
+                          Expected = new Polynomial(gf11, 0, 0, 0, 0, 0, 0, 1, 1, 10, 6)
+                      },
+                      new ComplementaryPolynomialBuildingTestCase
+                      {
+                          SourceFilter = new Polynomial(gf17, 10, 16, 5, 0, 0, 0, 0, 16),
+                          MaxFilterLength = 16,
+                          Expected = new Polynomial(gf17, 12, 4, 0, 15, 0, 1)
+                      }
+                  };
         }
 
         public GcdBasedBuilderTests()
@@ -59,9 +74,9 @@
 
         [Theory]
         [MemberData(nameof(BuildTestsData))]
-        public void ShouldBuildComplementaryFilter(Polynomial sourceFilter, int maxFilterLength, Polynomial expectedComplementaryFilter)
+        public void ShouldBuildComplementaryFilter(ComplementaryPolynomialBuildingTestCase testCase)
         {
-            Assert.Equal(expectedComplementaryFilter, _builder.Build(sourceFilter, maxFilterLength));
+            Assert.Equal(testCase.Expected, _builder.Build(testCase.SourceFilter, testCase.MaxFilterLength));
         }
     }
 }
