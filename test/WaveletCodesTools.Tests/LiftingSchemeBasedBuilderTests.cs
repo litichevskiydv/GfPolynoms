5f652fff3d0edec7096bd8986a69ccc578616415
dmitriy.litichevskiy <litichevskiydv@gmail.com>
2019-07-14 23:44:08 +0500

    TheoryData was used in LiftingSchemeBasedBuilder tests

--- test/WaveletCodesTools.Tests/LiftingSchemeBasedBuilderTests.cs
+++ test/WaveletCodesTools.Tests/LiftingSchemeBasedBuilderTests.cs
@@ -1,6 +1,5 @@
 ﻿namespace AppliedAlgebra.WaveletCodesTools.Tests
 {
-    using System.Collections.Generic;
     using GeneratingPolynomialsBuilder;
     using GfAlgorithms.ComplementaryFilterBuilder;
     using GfAlgorithms.LinearSystemSolver;
@@ -8,6 +7,7 @@
     using GfPolynoms;
     using GfPolynoms.GaloisFields;
     using JetBrains.Annotations;
+    using TestCases;
     using Xunit;
 
     public class LiftingSchemeBasedBuilderTests
@@ -15,7 +15,7 @@
         private readonly LiftingSchemeBasedBuilder _builder;
 
         [UsedImplicitly]
-        public static readonly IEnumerable<object[]> BuildTestsData;
+        public static readonly TheoryData<GeneratingPolynomialsBuilderTestCase> BuildTestsData;
 
         static LiftingSchemeBasedBuilderTests()
         {
@@ -28,51 +28,51 @@
             var gf32 = new PrimePowerOrderField(32, new Polynomial(new PrimeOrderField(2), 1, 0, 0, 1, 0, 1));
 
             BuildTestsData =
-                new[]
+                new TheoryData<GeneratingPolynomialsBuilderTestCase>
                 {
-                    new object[]
+                    new GeneratingPolynomialsBuilderTestCase
                     {
-                        7, 3, new Polynomial(gf8, 3, 2, 7, 6, 4, 2)
+                        N = 7, D = 3, SourceFilter = new Polynomial(gf8, 3, 2, 7, 6, 4, 2)
                     },
-                    new object[]
+                    new GeneratingPolynomialsBuilderTestCase
                     {
-                        7, 4, new Polynomial(gf8, 3, 2, 7, 6, 4, 2)
+                        N = 7, D = 4, SourceFilter = new Polynomial(gf8, 3, 2, 7, 6, 4, 2)
                     },
-                    new object[]
+                    new GeneratingPolynomialsBuilderTestCase
                     {
-                        8, 4, new Polynomial(gf9, 1, 2, 3, 2, 2, 3, 2, 1)
+                        N = 8, D = 4, SourceFilter = new Polynomial(gf9, 1, 2, 3, 2, 2, 3, 2, 1)
                     },
-                    new object[]
+                    new GeneratingPolynomialsBuilderTestCase
                     {
-                        10, 6, new Polynomial(gf11, 0, 0, 0, 0, 0, 10, 5, 4, 3, 4)
+                        N = 10, D = 6, SourceFilter = new Polynomial(gf11, 0, 0, 0, 0, 0, 10, 5, 4, 3, 4)
                     },
-                    new object[]
+                    new GeneratingPolynomialsBuilderTestCase
                     {
-                        10, 5, new Polynomial(gf11, 0, 0, 0, 0, 0, 10, 5, 4, 3, 4)
+                        N = 10, D = 5, SourceFilter = new Polynomial(gf11, 0, 0, 0, 0, 0, 10, 5, 4, 3, 4)
                     },
-                    new object[]
+                    new GeneratingPolynomialsBuilderTestCase
                     {
-                        12, 6, new Polynomial(gf13, 0, 0, 0, 0, 0, 10, 5, 4, 3, 4, 11, 9)
+                        N = 12, D = 6, SourceFilter = new Polynomial(gf13, 0, 0, 0, 0, 0, 10, 5, 4, 3, 4, 11, 9)
                     },
-                    new object[]
+                    new GeneratingPolynomialsBuilderTestCase
                     {
-                        15, 7, new Polynomial(gf16, 3, 2, 7, 6, 4, 2, 11, 7, 5)
+                        N = 15, D = 7, SourceFilter = new Polynomial(gf16, 3, 2, 7, 6, 4, 2, 11, 7, 5)
                     },
-                    new object[]
+                    new GeneratingPolynomialsBuilderTestCase
                     {
-                        15, 8, new Polynomial(gf16, 3, 2, 7, 6, 4, 2, 11, 7, 5)
+                        N = 15, D = 8, SourceFilter = new Polynomial(gf16, 3, 2, 7, 6, 4, 2, 11, 7, 5)
                     },
-                    new object[]
+                    new GeneratingPolynomialsBuilderTestCase
                     {
-                        26, 12, new Polynomial(gf27, 0, 0, 0, 1, 2, 3, 4, 1, 6, 7, 8, 9, 1, 10, 1, 12, 1, 14, 1, 17, 1, 19, 20, 1, 1, 1, 22)
+                        N = 26, D = 12, SourceFilter = new Polynomial(gf27, 0, 0, 0, 1, 2, 3, 4, 1, 6, 7, 8, 9, 1, 10, 1, 12, 1, 14, 1, 17, 1, 19, 20, 1, 1, 1, 22)
                     },
-                    new object[]
+                    new GeneratingPolynomialsBuilderTestCase
                     {
-                        31, 16, new Polynomial(gf32, 23, 13, 27, 1, 15, 13, 1, 16, 1, 21, 28, 30, 12, 19, 17, 4, 1, 19, 14, 0, 3, 5, 6)
+                        N = 31, D = 16, SourceFilter = new Polynomial(gf32, 23, 13, 27, 1, 15, 13, 1, 16, 1, 21, 28, 30, 12, 19, 17, 4, 1, 19, 14, 0, 3, 5, 6)
                     },
-                    new object[]
+                    new GeneratingPolynomialsBuilderTestCase
                     {
-                        31, 15, new Polynomial(gf32, 23, 13, 27, 1, 15, 13, 1, 16, 1, 21, 28, 30, 12, 19, 17, 4, 1, 19, 14, 0, 3, 5, 6)
+                        N = 31, D = 15, SourceFilter = new Polynomial(gf32, 23, 13, 27, 1, 15, 13, 1, 16, 1, 21, 28, 30, 12, 19, 17, 4, 1, 19, 14, 0, 3, 5, 6)
                     }
                 };
         }
@@ -84,19 +84,19 @@
 
         [Theory]
         [MemberData(nameof(BuildTestsData))]
-        public void ShouldBuildGeneratingPolynomial(int n, int d, Polynomial sourceFilter)
+        public void ShouldBuildGeneratingPolynomial(GeneratingPolynomialsBuilderTestCase testCase)
         {
             // When
-            var generatingPolynomial = _builder.Build(n, d, sourceFilter);
+            var generatingPolynomial = _builder.Build(testCase.N, testCase.D, testCase.SourceFilter);
 
             // Then
-            var field = sourceFilter.Field;
+            var field = testCase.SourceFilter.Field;
             var zeroValuesCount = 0;
             var nonZeroValuesCount = 0;
 
             var i = 0;
-            var j = n - 1;
-            for (; i < n && generatingPolynomial.Evaluate(field.GetGeneratingElementPower(i)) == 0; i++)
+            var j = testCase.N - 1;
+            for (; i < testCase.N && generatingPolynomial.Evaluate(field.GetGeneratingElementPower(i)) == 0; i++)
                 zeroValuesCount++;
             for (; j > i && generatingPolynomial.Evaluate(field.GetGeneratingElementPower(j)) == 0; j--)
                 zeroValuesCount++;
@@ -104,7 +104,13 @@
                 if (generatingPolynomial.Evaluate(field.GetGeneratingElementPower(i)) != 0)
                     nonZeroValuesCount++;
 
-            Assert.True(d - 1 <= zeroValuesCount && (n % 2 == 0 && nonZeroValuesCount >= n / 2 || n % 2 == 1 && nonZeroValuesCount >= (n - 1) / 2));
+            Assert.True(
+                testCase.D - 1 <= zeroValuesCount
+                && (
+                    testCase.N % 2 == 0 && nonZeroValuesCount >= testCase.N / 2
+                    || testCase.N % 2 == 1 && nonZeroValuesCount >= (testCase.N - 1) / 2
+                )
+            );
         }
     }
 }
---
+++ test/WaveletCodesTools.Tests/TestCases/GeneratingPolynomialsBuilderTestCase.cs
@@ -0,0 +1,13 @@
+﻿namespace AppliedAlgebra.WaveletCodesTools.Tests.TestCases
+{
+    using GfPolynoms;
+
+    public class GeneratingPolynomialsBuilderTestCase
+    {
+        public int N { get; set; }
+
+        public int D { get; set; }
+
+        public Polynomial SourceFilter { get; set; }
+    }
+}
