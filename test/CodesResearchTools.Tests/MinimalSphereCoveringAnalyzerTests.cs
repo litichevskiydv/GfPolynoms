3f5dcadd200c01f9798ba3ca5df788f6187a7496
dmitriy.litichevskiy <litichevskiydv@gmail.com>
2019-11-05 01:45:41 +0300

    Test was simplified

--- test/CodesResearchTools.Tests/MinimalSphereCoveringAnalyzerTests.cs
+++ test/CodesResearchTools.Tests/MinimalSphereCoveringAnalyzerTests.cs
@@ -1,8 +1,11 @@
 ﻿namespace AppliedAlgebra.CodesResearchTools.Tests
 {
-    using System;
+    using System.Collections.Generic;
     using Analyzers.CodeSpaceCovering;
-    using GolayCodesTools;
+    using CodesAbstractions;
+    using GfPolynoms;
+    using GfPolynoms.Extensions;
+    using GfPolynoms.GaloisFields;
     using Microsoft.Extensions.Logging;
     using Moq;
     using NoiseGenerator;
@@ -10,6 +13,43 @@
 
     public class MinimalSphereCoveringAnalyzerTests
     {
+        private class FakeCode : ICode
+        {
+            private readonly PrimeOrderField _gf2 = new PrimeOrderField(2);
+
+            public GaloisField Field => _gf2;
+            public int CodewordLength => 3;
+            public int InformationWordLength => 2;
+            public int CodeDistance => 0;
+
+            public FieldElement[] Encode(FieldElement[] informationWord)
+            {
+                return new[] { informationWord[0], informationWord[1], informationWord[0] + informationWord[1] };
+            }
+
+            public FieldElement[] Decode(FieldElement[] noisyCodeword)
+            {
+                return new[] { _gf2.One(), _gf2.One() };
+            }
+
+            public IReadOnlyList<FieldElement[]> DecodeViaList(FieldElement[] noisyCodeword, int? listDecodingRadius = null)
+            {
+                var list = new List<FieldElement[]> { new[] { noisyCodeword[0], noisyCodeword[1] } };
+
+                if (Equals(noisyCodeword[0] + noisyCodeword[1], noisyCodeword[2]) == false || listDecodingRadius == 2)
+                {
+                    list.Add(new[] { noisyCodeword[0] + _gf2.One(), noisyCodeword[1] });
+                    list.Add(new[] { noisyCodeword[0], noisyCodeword[1] + _gf2.One() });
+                }
+
+                if (Equals(noisyCodeword[0] + noisyCodeword[1], noisyCodeword[2]) && listDecodingRadius == 2)
+                    list.Add(new[] { noisyCodeword[0] + _gf2.One(), noisyCodeword[1] + _gf2.One() });
+
+
+                return list;
+            }
+        }
+
         private readonly Mock<ILogger<MinimalSphereCoveringAnalyzer>> _mockLogger;
         private readonly MinimalSphereCoveringAnalyzer _analyzer;
 
@@ -23,17 +63,14 @@
         public void ShouldAnalyzeSphereCovering()
         {
             // Given
-            var g11 = new G11GolayCode();
+            var code = new FakeCode();
 
             // When
-            var actualMinimalRadius = _analyzer.Analyze(g11);
+            var actualMinimalRadius = _analyzer.Analyze(code);
 
             // Then
-            const int expectedMinimalRadius = 2;
+            const int expectedMinimalRadius = 1;
             Assert.Equal(expectedMinimalRadius, actualMinimalRadius);
-
-            Assert.All(_mockLogger.Invocations, x => Assert.Equal(LogLevel.Information, x.Arguments[0]));
-            Assert.True(_mockLogger.Invocations.Count >= 1);
         }
     }
 }
