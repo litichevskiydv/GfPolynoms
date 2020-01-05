9c9c8db14dc44dc3ecfd9ddabea5aea2d0af2d2a
dmitriy.litichevskiy <litichevskiydv@gmail.com>
2020-01-06 00:47:35 +0300

    Load balancing in CommonSpectrumAnalyzer was improved

--- src/CodesResearchTools/Analyzers/Spectrum/CommonSpectrumAnalyzer.cs
+++ src/CodesResearchTools/Analyzers/Spectrum/CommonSpectrumAnalyzer.cs
@@ -37,6 +37,34 @@
             return Analyze(code.Field, code.InformationWordLength, code.Encode, options);
         }
 
+        private Dictionary<int, BigInteger> ProcessInformationWord(
+            Func<FieldElement[], FieldElement[]> encodingProcedure,
+            long loggingResolution,
+            ref long processedInformationWords,
+            Dictionary<int, BigInteger> localSpectrum,
+            FieldElement[] informationWord
+        )
+        {
+            if (informationWord.All(x => x.Representation == 0))
+                return localSpectrum;
+
+            var codewordWeight = encodingProcedure(informationWord).Count(x => x.Representation != 0);
+
+            if (localSpectrum.ContainsKey(codewordWeight) == false)
+                localSpectrum[codewordWeight] = 0;
+            localSpectrum[codewordWeight]++;
+
+            if (Interlocked.Increment(ref processedInformationWords) % loggingResolution == 0)
+                _logger.LogInformation(
+                    "Thread {threadId}, processed {processedInformationWords} pairs, local spectrum {localSpectrum}",
+                    Thread.CurrentThread.ManagedThreadId,
+                    processedInformationWords,
+                    string.Join(", ", localSpectrum.OrderBy(x => x.Key).Select(x => $"(W: {x.Key}, C: {x.Value})"))
+                );
+
+            return localSpectrum;
+        }
+
         public IReadOnlyDictionary<int, BigInteger> Analyze(
             GaloisField field,
             int informationWordLength,
@@ -52,28 +80,25 @@
                 throw new ArgumentNullException(nameof(encodingProcedure));
 
             var opts = options ?? new SpectrumAnalyzerOptions();
+            var firstPortionLength = Math.Min((int) Math.Ceiling(Math.Log(opts.MaxDegreeOfParallelism, field.Order)), informationWordLength);
+            var secondPortionLength = informationWordLength - firstPortionLength;
 
-            var processedCodewords = 0L;
+            var processedInformationWords = 0L;
             var spectrum = new ConcurrentDictionary<int, BigInteger>();
             Parallel.ForEach(
-                _variantsIterator.IterateVectors(field, informationWordLength).Skip(1),
+                _variantsIterator.IterateVectors(field, firstPortionLength),
                 new ParallelOptions {MaxDegreeOfParallelism = opts.MaxDegreeOfParallelism},
                 () => new Dictionary<int, BigInteger>(),
-                (informationWord, loopState, localSpectrum) =>
+                (informationWordFirstPart, loopState, localSpectrum) =>
                 {
-                    var codewordWeight = encodingProcedure(informationWord).Count(x => x.Representation != 0);
+                    if (secondPortionLength == 0)
+                        return ProcessInformationWord(encodingProcedure, opts.LoggingResolution, ref processedInformationWords, localSpectrum, informationWordFirstPart);
 
-                    if (localSpectrum.ContainsKey(codewordWeight) == false)
-                        localSpectrum[codewordWeight] = 0;
-                    localSpectrum[codewordWeight]++;
-
-                    if (Interlocked.Increment(ref processedCodewords) % opts.LoggingResolution == 0)
-                        _logger.LogInformation(
-                            "Thread {threadId}, processed {processedCodewords} pairs, local spectrum {localSpectrum}",
-                            Thread.CurrentThread.ManagedThreadId,
-                            processedCodewords,
-                            string.Join(", ", localSpectrum.OrderBy(x => x.Key).Select(x => $"(W: {x.Key}, C: {x.Value})"))
-                        );
+                    foreach (var informationWordSecondPart in _variantsIterator.IterateVectors(field, secondPortionLength))
+                    {
+                        var informationWord = informationWordFirstPart.Concat(informationWordSecondPart).ToArray();
+                        ProcessInformationWord(encodingProcedure, opts.LoggingResolution, ref processedInformationWords, localSpectrum, informationWord);
+                    }
 
                     return localSpectrum;
                 },
