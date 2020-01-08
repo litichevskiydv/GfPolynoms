18157a67c2e0ef3e175e89fd2542e4067c206fd3
dmitriy.litichevskiy <litichevskiydv@gmail.com>
2020-01-08 17:08:06 +0300

    Words processing strategy has been changed

--- src/CodesResearchTools/Analyzers/CodeSpaceCovering/MinimalSphereCoveringAnalyzer.cs
+++ src/CodesResearchTools/Analyzers/CodeSpaceCovering/MinimalSphereCoveringAnalyzer.cs
@@ -7,6 +7,7 @@
     using CodesAbstractions;
     using GfAlgorithms.Extensions;
     using GfAlgorithms.VariantsIterator;
+    using GfPolynoms;
     using Microsoft.Extensions.Logging;
 
     public class MinimalSphereCoveringAnalyzer : IMinimalSphereCoveringAnalyzer
@@ -14,30 +15,51 @@
         private readonly IVariantsIterator _variantsIterator;
         private readonly ILogger _logger;
 
+        private int ProcessWord(
+            ICode code,
+            MinimalSphereCoveringAnalyzerOptions options,
+            ref long processedWordsCount,
+            int minimalSphereCoveringRadius,
+            FieldElement[] word
+        )
+        {
+            var codewordsInLargestBall = code.DecodeViaList(word, code.CodewordLength - 1).Select(code.Encode);
+            if (Interlocked.Increment(ref processedWordsCount) % options.LoggingResolution == 0)
+                _logger.LogInformation(
+                    "Thread [{managedThreadId}]: processed {processedWordsCount} words, local minimal radius {minimalSphereCoveringRadius}",
+                    Thread.CurrentThread.ManagedThreadId, processedWordsCount, minimalSphereCoveringRadius
+                );
+
+            return Math.Max(minimalSphereCoveringRadius, codewordsInLargestBall.Min(x => x.ComputeHammingDistance(word)));
+        }
+
         public int Analyze(ICode code, MinimalSphereCoveringAnalyzerOptions options = null)
         {
             if (code == null)
                 throw new ArgumentNullException(nameof(code));
 
             var opts = options ?? new MinimalSphereCoveringAnalyzerOptions();
+            var firstPortionLength = Math.Min(
+                Math.Max((int)Math.Ceiling(Math.Log(opts.MaxDegreeOfParallelism, code.Field.Order)), 1),
+                code.CodewordLength
+            );
+            var secondPortionLength = code.CodewordLength - firstPortionLength;
 
             var minimalRadius = 0;
             var processedWordsCount = 0L;
             var syncRoot = new object();
             Parallel.ForEach(
-                _variantsIterator.IterateVectors(code.Field, code.CodewordLength),
+                _variantsIterator.IterateVectors(code.Field, firstPortionLength),
                 new ParallelOptions {MaxDegreeOfParallelism = opts.MaxDegreeOfParallelism},
                 () => 0,
-                (word, loopState, localMinimalRadius) =>
+                (wordFirstPart, loopState, localMinimalRadius) =>
                 {
-                    var codewordsInLargestBall = code.DecodeViaList(word, code.CodewordLength - 1).Select(code.Encode);
-                    if (Interlocked.Increment(ref processedWordsCount) % opts.LoggingResolution == 0)
-                        _logger.LogInformation(
-                            "Thread [{managedThreadId}]: processed {processedWordsCount} words, local minimal radius {localMinimalRadius}",
-                            Thread.CurrentThread.ManagedThreadId, processedWordsCount, localMinimalRadius
-                        );
+                    if (secondPortionLength == 0)
+                        return ProcessWord(code, opts, ref processedWordsCount, localMinimalRadius, wordFirstPart);
 
-                    return Math.Max(localMinimalRadius, codewordsInLargestBall.Min(x => x.ComputeHammingDistance(word)));
+                    return _variantsIterator.IterateVectors(code.Field, secondPortionLength)
+                        .Select(wordSecondPart => wordFirstPart.Concat(wordSecondPart).ToArray())
+                        .Aggregate(localMinimalRadius, (current, word) => ProcessWord(code, opts, ref processedWordsCount, current, word));
                 },
                 localMinimalRadius =>
                 {
