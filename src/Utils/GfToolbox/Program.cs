d9be6007b2084ab1e18ba45d524dea778f69f1c6
dmitriy.litichevskiy <litichevskiydv@gmail.com>
2020-02-29 02:09:40 +0300

    Basic initialization was made

--- src/Utils/GfToolbox/Program.cs
+++ src/Utils/GfToolbox/Program.cs
@@ -1,12 +1,47 @@
-﻿using System;
-
-namespace GfToolbox
+﻿namespace AppliedAlgebra.GfToolbox
 {
-    class Program
+    using System;
+    using System.CommandLine;
+    using System.Reflection;
+    using JetBrains.Annotations;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.Logging;
+    using Serilog;
+    using ILogger = Microsoft.Extensions.Logging.ILogger;
+
+    [UsedImplicitly]
+    public class Program
     {
+        private static readonly ILoggerFactory LoggerFactory;
+        private static readonly ILogger Logger;
+
+        static Program()
+        {
+            LoggerFactory = new LoggerFactory()
+                .AddSerilog(
+                    new LoggerConfiguration()
+                        .ReadFrom.Configuration(new ConfigurationBuilder().AddJsonFile("appsettings.json").Build())
+                        .Enrich.FromLogContext()
+                        .Enrich.WithProperty("Version", Assembly.GetEntryAssembly().GetName().Version.ToString(4))
+                        .CreateLogger()
+                );
+            Logger = LoggerFactory.CreateLogger<Program>();
+        }
+
+        [UsedImplicitly]
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                new RootCommand("Command line interface for algorithms over finite fields")
+                {
+                    new Command("polyphase-representation", "Computes polyphase representation for the given polynomial")
+                }.Invoke(args);
+            }
+            catch (Exception exception)
+            {
+                Logger.LogError(exception, "Exception occurred during the execution");
+            }
         }
     }
 }
