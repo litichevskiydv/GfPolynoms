9cef6877aa9a40a605158e7a47ef3c7ea5ed1b66
dmitriy.litichevskiy <litichevskiydv@gmail.com>
2020-03-01 14:50:53 +0300

    Internal visibility was configured for core library

--- test/GfPolynoms.Tests/GaluaFields/PrimeOrderFieldTests.cs
+++ test/GfPolynoms.Tests/GaluaFields/PrimeOrderFieldTests.cs
@@ -6,11 +6,11 @@
 
     public class PrimeOrderFieldTests
     {
-        private readonly GaloisField _gf5;
+        private readonly PrimeOrderField _gf5;
 
         public PrimeOrderFieldTests()
         {
-            _gf5 = GaloisField.Create(5);
+            _gf5 = (PrimeOrderField)GaloisField.Create(5);
         }
 
         [Theory]
