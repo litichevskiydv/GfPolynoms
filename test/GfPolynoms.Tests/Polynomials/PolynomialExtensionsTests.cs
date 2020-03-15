39b63b0b73921f71a077f5ae7c09ed13a2966d7f
dmitriy.litichevskiy <litichevskiydv@gmail.com>
2020-03-16 00:54:47 +0300

    Parameters validation in polynomial transferring to subfield was tested

--- test/GfPolynoms.Tests/Polynomials/PolynomialExtensionsTests.cs
+++ test/GfPolynoms.Tests/Polynomials/PolynomialExtensionsTests.cs
@@ -1,12 +1,39 @@
 ﻿namespace AppliedAlgebra.GfPolynoms.Tests.Polynomials
 {
+    using System;
     using System.Linq;
     using Extensions;
     using GaloisFields;
+    using JetBrains.Annotations;
+    using TestCases;
     using Xunit;
 
     public class PolynomialExtensionsTests
     {
+        [UsedImplicitly]
+        public static TheoryData<ChangeFieldParametersValidationTestCase> TransferToSubfieldParametersValidationTestCases;
+
+        static PolynomialExtensionsTests()
+        {
+            TransferToSubfieldParametersValidationTestCases
+                = new TheoryData<ChangeFieldParametersValidationTestCase>
+                  {
+                      new ChangeFieldParametersValidationTestCase {NewField = GaloisField.Create(3)},
+                      new ChangeFieldParametersValidationTestCase {Polynomial = new Polynomial(GaloisField.Create(3), 1)},
+                      new ChangeFieldParametersValidationTestCase
+                      {
+                          Polynomial = new Polynomial(GaloisField.Create(9), 1, 0, 2),
+                          NewField = GaloisField.Create(27)
+                      },
+                      new ChangeFieldParametersValidationTestCase
+                      {
+                          Polynomial = new Polynomial(GaloisField.Create(3), 1, 0, 2),
+                          NewField = GaloisField.Create(5)
+                      }
+                  };
+        }
+
+
         [Fact]
         public void ShouldGetPolynomialSpectrum()
         {
@@ -20,6 +47,13 @@
             // Then
             var expectedSpectrum = new[] { 3, 2, 1, 3 }.Select(x => gf5.CreateElement(x)).ToArray();
             Assert.Equal(expectedSpectrum, actualSpectrum);
+        }
+
+        [Theory]
+        [MemberData(nameof(TransferToSubfieldParametersValidationTestCases))]
+        public void TransferToSubfieldMustValidateParameters(ChangeFieldParametersValidationTestCase testCase)
+        {
+            Assert.ThrowsAny<ArgumentException>(() => testCase.Polynomial.TransferToSubfield(testCase.NewField));
         }
     }
 }
