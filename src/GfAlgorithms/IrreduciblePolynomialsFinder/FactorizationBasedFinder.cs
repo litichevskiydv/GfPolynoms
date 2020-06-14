c3f07124c78f4d718f9eebc3f8572fa7756c3199
dmitriy.litichevskiy <litichevskiydv@gmail.com>
2020-06-14 15:33:56 +0300

    Irreducible polynomials finder based on factorization process was implemented

---
+++ src/GfAlgorithms/IrreduciblePolynomialsFinder/FactorizationBasedFinder.cs
@@ -0,0 +1,42 @@
+﻿namespace AppliedAlgebra.GfAlgorithms.IrreduciblePolynomialsFinder
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Extensions;
+    using GfPolynoms;
+    using GfPolynoms.Extensions;
+    using GfPolynoms.GaloisFields;
+    using GfPolynoms.IrreduciblePolynomialsFinder;
+    using PolynomialsFactorizer;
+
+    /// <summary>
+    /// Irreducible polynomials finder based on factorization procedure
+    /// </summary>
+    public class FactorizationBasedFinder : IIrreduciblePolynomialsFinder
+    {
+        private readonly IPolynomialsFactorizer _polynomialsFactorizer;
+
+        public FactorizationBasedFinder(IPolynomialsFactorizer polynomialsFactorizer)
+        {
+            if (polynomialsFactorizer == null)
+                throw new ArgumentNullException(nameof(polynomialsFactorizer));
+
+            _polynomialsFactorizer = polynomialsFactorizer;
+        }
+
+
+        /// <inheritdoc />
+        public IEnumerable<Polynomial> Find(GaloisField field, int degree)
+        {
+            if (field == null)
+                throw new ArgumentNullException(nameof(field));
+            if (degree < 2)
+                throw new ArgumentException(nameof(degree));
+
+            var one = new Polynomial(field.One());
+            var polynomial = (one >> field.Order.Pow(degree)) - (one >> 1);
+            return _polynomialsFactorizer.Factorize(polynomial).Where(x => x.factor.Degree == degree).Select(x => x.factor);
+        }
+    }
+}
---
+++ test/GfAlgorithms.Tests/FactorizationBasedFinderTests.cs
@@ -0,0 +1,78 @@
+﻿namespace AppliedAlgebra.GfAlgorithms.Tests
+{
+    using System;
+    using System.Linq;
+    using GfPolynoms.GaloisFields;
+    using IrreduciblePolynomialsFinder;
+    using JetBrains.Annotations;
+    using PolynomialsFactorizer;
+    using PolynomialsGcdFinder;
+    using Xunit;
+
+    public class FindParametersValidationTestCase
+    {
+        public GaloisField Field { get; set; }
+
+        public int Degree { get; set; }
+    }
+
+    public class FactorizationBasedFinderTests
+    {
+        private readonly FactorizationBasedFinder _irreduciblePolynomialsFinder;
+
+        [UsedImplicitly] 
+        public static readonly TheoryData<FindParametersValidationTestCase> FindParametersValidationTestCase;
+
+        static FactorizationBasedFinderTests()
+        {
+            var gf2 = GaloisField.Create(2);
+
+            FindParametersValidationTestCase
+                = new TheoryData<FindParametersValidationTestCase>
+                  {
+                      new FindParametersValidationTestCase {Degree = 2},
+                      new FindParametersValidationTestCase {Field = gf2, Degree = -1},
+                      new FindParametersValidationTestCase {Field = gf2, Degree = 1}
+                  };
+        }
+
+        public FactorizationBasedFinderTests()
+        {
+            _irreduciblePolynomialsFinder = new FactorizationBasedFinder(new BerlekampFactorizer(new RecursiveGcdFinder()));
+        }
+
+        [Theory]
+        [MemberData(nameof(FindParametersValidationTestCase))]
+        public void FindMustValidateParameters(FindParametersValidationTestCase testCase)
+        {
+            Assert.ThrowsAny<ArgumentException>(() => _irreduciblePolynomialsFinder.Find(testCase.Field, testCase.Degree));
+        }
+
+        [Theory]
+        [InlineData(2, 2)]
+        [InlineData(2, 3)]
+        [InlineData(3, 2)]
+        [InlineData(3, 3)]
+        [InlineData(3, 4)]
+        [InlineData(5, 3)]
+        public void ShouldFindIrreduciblePolynomials(int fieldOrder, int degree)
+        {
+            // When
+            var irreduciblePolynomials = _irreduciblePolynomialsFinder.Find(GaloisField.Create(fieldOrder), degree);
+
+            // Then
+            Assert.All(irreduciblePolynomials,
+                polynomial =>
+                {
+                    Assert.Equal(degree, polynomial.Degree);
+                    Assert.NotNull(
+                        GaloisField.Create(
+                            (int)Math.Pow(fieldOrder, degree),
+                            Enumerable.Range(0, polynomial.Degree + 1).Select(x => polynomial[x]).ToArray()
+                        )
+                    );
+                }
+            );
+        }
+    }
+}
