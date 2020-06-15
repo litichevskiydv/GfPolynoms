910560a68b35506e514425de1dd8eba646cd3ecc
dmitriy.litichevskiy <litichevskiydv@gmail.com>
2020-06-15 23:04:36 +0300

    Tets for orthogonal filters generation were added

---
+++ test/GfAlgorithms.Tests/TestCases/WaveletTransform/OrthogonalIterationFiltersVectorsCalculationTestCase.cs
@@ -0,0 +1,28 @@
+﻿namespace AppliedAlgebra.GfAlgorithms.Tests.TestCases.WaveletTransform
+{
+    using GfPolynoms;
+
+    public class OrthogonalIterationFiltersVectorsCalculationTestCase
+    {
+        public int IterationNumber { get; }
+
+        public FieldElement[] SourceFilterH { get; }
+
+        public FieldElement[] SourceFilterG { get; }
+
+        public FieldElement Multiplier { get; }
+
+        public OrthogonalIterationFiltersVectorsCalculationTestCase(
+            int iterationNumber,
+            FieldElement[] sourceFilterH,
+            FieldElement[] sourceFilterG,
+            FieldElement multiplier = null
+        )
+        {
+            IterationNumber = iterationNumber;
+            SourceFilterH = sourceFilterH;
+            SourceFilterG = sourceFilterG;
+            Multiplier = multiplier;
+        }
+    }
+}
--- test/GfAlgorithms.Tests/WaveletTransform/IterationFiltersCalculator/CaireGrossmanPoorCalculatorTests.cs
+++ test/GfAlgorithms.Tests/WaveletTransform/IterationFiltersCalculator/CaireGrossmanPoorCalculatorTests.cs
@@ -5,6 +5,7 @@
     using GfPolynoms.Extensions;
     using GfPolynoms.GaloisFields;
     using JetBrains.Annotations;
+    using Matrices;
     using TestCases.WaveletTransform;
     using Xunit;
 
@@ -15,13 +16,16 @@
 
         [UsedImplicitly]
         public static TheoryData<IterationFilterVectorCalculationTestCase> IterationFilterVectorCalculationTestCases;
+        [UsedImplicitly]
+        public static TheoryData<OrthogonalIterationFiltersVectorsCalculationTestCase> OrthogonalIterationFiltersVectorsCalculationTestCases;
 
         static CaireGrossmanPoorCalculatorTests()
         {
             var gf17 = GaloisField.Create(17);
 
             var hSource1 = Enumerable.Repeat(gf17.CreateElement(16), 2).Concat(Enumerable.Repeat(gf17.Zero(), 14)).ToArray();
-            var gSource1 = new[] {gf17.One(), gf17.CreateElement(16)}.Concat(Enumerable.Repeat(gf17.Zero(), 14)).ToArray();
+            var gSource1 = new[] { gf17.One(), gf17.CreateElement(16) }.Concat(Enumerable.Repeat(gf17.Zero(), 14)).ToArray();
+
             IterationFilterVectorCalculationTestCases
                 = new TheoryData<IterationFilterVectorCalculationTestCase>
                   {
@@ -82,6 +86,15 @@
 
                       }
                   };
+
+            OrthogonalIterationFiltersVectorsCalculationTestCases
+                = new TheoryData<OrthogonalIterationFiltersVectorsCalculationTestCase>
+                  {
+                    new OrthogonalIterationFiltersVectorsCalculationTestCase(1, hSource1, gSource1, gf17.CreateElement(2)),
+                    new OrthogonalIterationFiltersVectorsCalculationTestCase(2, hSource1, gSource1, gf17.CreateElement(2)),
+                    new OrthogonalIterationFiltersVectorsCalculationTestCase(3, hSource1, gSource1, gf17.CreateElement(2)),
+                    new OrthogonalIterationFiltersVectorsCalculationTestCase(4, hSource1, gSource1, gf17.CreateElement(2)),
+                  };
         }
 
         public CaireGrossmanPoorCalculatorTests()
@@ -98,6 +111,36 @@
 
             // Then
             Assert.Equal(testCase.ExpectedIterationFilter, actualIterationFilter);
+        }
+
+        [Theory]
+        [MemberData(nameof(OrthogonalIterationFiltersVectorsCalculationTestCases))]
+        public void MustCalculateOrthogonalIterationFiltersVectors(OrthogonalIterationFiltersVectorsCalculationTestCase testCase)
+        {
+            // Given
+            var field = testCase.SourceFilterH[0].Field;
+            var multiplier = testCase.Multiplier ?? field.One();
+
+            // When
+            var iterationFilterH = _iterationFiltersCalculator.GetIterationFilter(testCase.IterationNumber, testCase.SourceFilterH);
+            var iterationFilterG = _iterationFiltersCalculator.GetIterationFilter(testCase.IterationNumber, testCase.SourceFilterG);
+
+            // Then
+            var hMatrix = FieldElementsMatrix.DoubleCirculantMatrix(iterationFilterH);
+            var hMatrixTransposed = FieldElementsMatrix.Transpose(hMatrix);
+            var gMatrix = FieldElementsMatrix.DoubleCirculantMatrix(iterationFilterG);
+            var gMatrixTransposed = FieldElementsMatrix.Transpose(gMatrix);
+
+
+            var fullSizeIdentityMatrix = FieldElementsMatrix.IdentityMatrix(hMatrix.Field, iterationFilterH.Length);
+            Assert.Equal(multiplier * fullSizeIdentityMatrix, hMatrixTransposed * hMatrix + gMatrixTransposed * gMatrix);
+
+            var halfSizeIdentityMatrix = FieldElementsMatrix.IdentityMatrix(hMatrix.Field, iterationFilterH.Length / 2);
+            var halfSizeZeroMatrix = FieldElementsMatrix.ZeroMatrix(hMatrix.Field, iterationFilterH.Length / 2);
+            Assert.Equal(multiplier * halfSizeIdentityMatrix, hMatrix * hMatrixTransposed);
+            Assert.Equal(multiplier * halfSizeIdentityMatrix, gMatrix * gMatrixTransposed);
+            Assert.Equal(halfSizeZeroMatrix, hMatrix * gMatrixTransposed);
+            Assert.Equal(halfSizeZeroMatrix, gMatrix * hMatrixTransposed);
         }
     }
 }
