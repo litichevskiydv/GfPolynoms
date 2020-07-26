e902e4a8c1577c9d9b0b3db5ed0c49d03e474991
dmitriy.litichevskiy <litichevskiydv@gmail.com>
2020-07-26 15:42:52 +0300

    Test cases for ConvolutionBasedCalculator were added

--- test/GfAlgorithms.Tests/WaveletTransform/IterationFiltersCalculator/ConvolutionBasedCalculatorTests.cs
+++ test/GfAlgorithms.Tests/WaveletTransform/IterationFiltersCalculator/ConvolutionBasedCalculatorTests.cs
@@ -1,6 +1,7 @@
 ﻿namespace AppliedAlgebra.GfAlgorithms.Tests.WaveletTransform.IterationFiltersCalculator
 {
     using GfAlgorithms.WaveletTransform.IterationFiltersCalculator;
+    using GfPolynoms;
     using GfPolynoms.Extensions;
     using GfPolynoms.GaloisFields;
     using JetBrains.Annotations;
@@ -18,10 +19,15 @@
         static ConvolutionBasedCalculatorTests()
         {
             var gf2 = GaloisField.Create(2);
+            var gf3 = GaloisField.Create(3);
             var hSource1 = gf2.CreateElementsVector(1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 1);
             var gSource1 = gf2.CreateElementsVector(1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 1);
             var hSource2 = gf2.CreateElementsVector(1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0);
             var gSource2 = gf2.CreateElementsVector(1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0);
+            var hSource3 = new Polynomial(gf3, 2, 1, 2, 1, 1, 0, 2, 1).GetCoefficients(11);
+            var gSource3 = new Polynomial(gf3, 1, 1, 2, 2, 2, 1).GetCoefficients(11);
+            var hWithTildeSource3 = new Polynomial(gf3, 1, 2, 0, 0, 0, 0, 0, 0, 1, 1, 2, 1).GetCoefficients(11);
+            var gWithTildeSource3 = new Polynomial(gf3, 2, 2, 0, 0, 0, 0, 2, 2, 0, 1, 2, 2).GetCoefficients(11);
 
 
             OrthogonalIterationFiltersVectorsCalculationTestCases
@@ -31,7 +37,9 @@
             ComplementaryIterationFiltersVectorsCalculationTestCases
                 = new ComplementaryIterationFiltersVectorsCalculationTheoryData(
                     new ComplementaryIterationFiltersVectorsCalculationTestCase(2, hSource2, gSource2),
-                    new ComplementaryIterationFiltersVectorsCalculationTestCase(3, hSource2, gSource2)
+                    new ComplementaryIterationFiltersVectorsCalculationTestCase(3, hSource2, gSource2),
+                    new ComplementaryIterationFiltersVectorsCalculationTestCase(2, hSource3, gSource3),
+                    new ComplementaryIterationFiltersVectorsCalculationTestCase(2, hWithTildeSource3, gWithTildeSource3)
                 );
         }
 
