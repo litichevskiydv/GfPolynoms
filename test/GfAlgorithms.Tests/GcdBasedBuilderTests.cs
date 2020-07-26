8d2e2d23dd9a1927bfbcca22508cee7feaad1eab
dmitriy.litichevskiy <litichevskiydv@gmail.com>
2020-07-26 15:25:48 +0300

    Test cases were corrected

--- test/GfAlgorithms.Tests/GcdBasedBuilderTests.cs
+++ test/GfAlgorithms.Tests/GcdBasedBuilderTests.cs
@@ -47,7 +47,7 @@
                       },
                       new ComplementaryPolynomialBuildingTestCase
                       {
-                          SourceFilter = new Polynomial(gf3, 2, 1, 0, 0, 1, 1),
+                          SourceFilter = new Polynomial(gf3, 2, 1, 2, 1, 1, 0, 2, 1),
                           MaxFilterLength = 12
                       },
                       new ComplementaryPolynomialBuildingTestCase
