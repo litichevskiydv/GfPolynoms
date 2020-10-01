f2395c4729e5402b2c69f9d597e9989884d6fd00
dmitriy.litichevskiy <litichevskiydv@gmail.com>
2020-10-01 09:51:08 +0300

    Contract for details vectors generator was described

--- src/WaveletCodesTools/Encoding/IMultilevelEncoder.cs
+++ src/WaveletCodesTools/Encoding/IMultilevelEncoder.cs
@@ -2,8 +2,18 @@
 {
     using GfPolynoms;
 
+    /// <summary>
+    /// Contract for the multilevel wavelet code encoder
+    /// </summary>
     public interface IMultilevelEncoder
     {
+        /// <summary>
+        /// Computes wavelet code codeword for information word <paramref name="informationWord"/>
+        /// </summary>
+        /// <param name="codewordLength">Codeword length</param>
+        /// <param name="synthesisFilters">Synthesis filters pair</param>
+        /// <param name="informationWord">Information word</param>
+        /// <returns>Codeword</returns>
         FieldElement[] Encode(
             int codewordLength,
             (FieldElement[] h, FieldElement[] g) synthesisFilters,
---
+++ src/WaveletCodesTools/Encoding/MultilevelEncoderDependencies/DetailsVectorsGenerator/IDetailsVectorsGenerator.cs
@@ -0,0 +1,19 @@
+﻿namespace AppliedAlgebra.WaveletCodesTools.Encoding.MultilevelEncoderDependencies.DetailsVectorsGenerator
+{
+    using GfPolynoms;
+
+    /// <summary>
+    /// Contract for details vectors generator
+    /// </summary>
+    public interface IDetailsVectorsGenerator
+    {
+        /// <summary>
+        /// Generates details vector for wavelet decomposition level <paramref name="levelNumber"/>
+        /// </summary>
+        /// <param name="informationWord">Information word</param>
+        /// <param name="levelNumber">Wavelet decomposition level number</param>
+        /// <param name="approximationVector">Approximation vector for level <paramref name="levelNumber"/></param>
+        /// <returns>Details vector</returns>
+        FieldElement[] GetDetailsVector(FieldElement[] informationWord, int levelNumber, FieldElement[] approximationVector);
+    }
+}
