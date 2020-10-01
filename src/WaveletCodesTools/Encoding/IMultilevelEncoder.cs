fd0b8997cfe877279e505cec0171b7dbd0562bdd
dmitriy.litichevskiy <litichevskiydv@gmail.com>
2020-10-01 08:57:28 +0300

    Contract for multilevel encoder was added

---
+++ src/WaveletCodesTools/Encoding/IMultilevelEncoder.cs
@@ -0,0 +1,13 @@
+﻿namespace AppliedAlgebra.WaveletCodesTools.Encoding
+{
+    using GfPolynoms;
+
+    public interface IMultilevelEncoder
+    {
+        FieldElement[] Encode(
+            int codewordLength,
+            (FieldElement[] h, FieldElement[] g) synthesisFilters,
+            FieldElement[] informationWord
+        );
+    }
+}
