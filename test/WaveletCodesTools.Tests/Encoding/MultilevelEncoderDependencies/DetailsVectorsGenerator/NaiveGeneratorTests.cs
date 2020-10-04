d352556d7670b95f7ac5e80f6b73e7d8ce47555a
dmitriy.litichevskiy <litichevskiydv@gmail.com>
2020-10-04 20:31:27 +0300

    Naive details vectors generator was implemented

---
+++ test/WaveletCodesTools.Tests/Encoding/MultilevelEncoderDependencies/DetailsVectorsGenerator/NaiveGeneratorTests.cs
@@ -0,0 +1,63 @@
+﻿namespace AppliedAlgebra.WaveletCodesTools.Tests.Encoding.MultilevelEncoderDependencies.DetailsVectorsGenerator
+{
+    using System;
+    using GfAlgorithms.Matrices;
+    using GfPolynoms.Extensions;
+    using GfPolynoms.GaloisFields;
+    using JetBrains.Annotations;
+    using TestCases;
+    using WaveletCodesTools.Encoding.MultilevelEncoderDependencies.DetailsVectorsGenerator;
+    using Xunit;
+
+    public class NaiveGeneratorTests
+    {
+        private readonly NaiveGenerator _detailsVectorsGenerator;
+
+        [UsedImplicitly]
+        public static readonly TheoryData<GetDetailsVectorParametersValidationTestCase> GetDetailsVectorParametersValidationTestCases;
+
+        static NaiveGeneratorTests()
+        {
+            GetDetailsVectorParametersValidationTestCases
+                = new TheoryData<GetDetailsVectorParametersValidationTestCase>
+                  {
+                      new GetDetailsVectorParametersValidationTestCase {LevelNumber = -1},
+                      new GetDetailsVectorParametersValidationTestCase {LevelNumber = 1}
+                  };
+        }
+
+        public NaiveGeneratorTests()
+        {
+            var gf3 = GaloisField.Create(3);
+            _detailsVectorsGenerator =
+                new NaiveGenerator(
+                    gf3.CreateElement(2) * FieldElementsMatrix.CirculantMatrix(gf3.Zero(), gf3.Zero(), gf3.One())
+                );
+        }
+
+        [Theory]
+        [MemberData(nameof(GetDetailsVectorParametersValidationTestCases))]
+        public void GetApproximationVectorMustValidateParameters(GetDetailsVectorParametersValidationTestCase testCase)
+        {
+            Assert.ThrowsAny<ArgumentException>(
+                () => _detailsVectorsGenerator.GetDetailsVector(testCase.InformationWord, testCase.LevelNumber, testCase.ApproximationVector)
+            );
+        }
+
+        [Fact]
+        public void MustGenerateDetailsVector()
+        {
+            // Given
+            var gf3 = GaloisField.Create(3);
+            var informationWord = new[] {gf3.Zero(), gf3.One(), gf3.CreateElement(2)};
+            const int levelNumber = 0;
+
+            // When
+            var actualDetailsVector = _detailsVectorsGenerator.GetDetailsVector(informationWord, levelNumber, informationWord);
+
+            // Then
+            var expectedDetailsVector = new[] {gf3.One(), gf3.Zero(), gf3.CreateElement(2)};
+            Assert.Equal(expectedDetailsVector, actualDetailsVector);
+        }
+    }
+}
---
+++ test/WaveletCodesTools.Tests/TestCases/GetDetailsVectorParametersValidationTestCase.cs
@@ -0,0 +1,13 @@
+﻿namespace AppliedAlgebra.WaveletCodesTools.Tests.TestCases
+{
+    using GfPolynoms;
+
+    public class GetDetailsVectorParametersValidationTestCase
+    {
+        public FieldElement[] InformationWord { get; set; }
+
+        public int LevelNumber { get; set; }
+
+        public FieldElement[] ApproximationVector { get; set; }
+    }
+}
