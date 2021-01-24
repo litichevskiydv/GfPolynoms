d816d7a2400581d3a920311de092725f3bff3ef3
dmitriy.litichevskiy <litichevskiydv@gmail.com>
2021-01-24 17:41:11 +0300

    Contract for linear codes' generating matrices provider was added

---
+++ src/WaveletCodesTools/Encoding/MultilevelEncoderDependencies/GeneratingMatrixProvider/IGeneratingMatrixProvider.cs
@@ -0,0 +1,16 @@
+﻿namespace AppliedAlgebra.WaveletCodesTools.Encoding.MultilevelEncoderDependencies.GeneratingMatrixProvider
+{
+    using GfAlgorithms.Matrices;
+
+    /// <summary>
+    /// Contract for linear codes' generating matrices provider
+    /// </summary>
+    public interface IGeneratingMatrixProvider
+    {
+        /// <summary>
+        /// Provides linear codes generating matrix
+        /// </summary>
+        /// <returns></returns>
+        FieldElementsMatrix GetGeneratingMatrix();
+    }
+}
