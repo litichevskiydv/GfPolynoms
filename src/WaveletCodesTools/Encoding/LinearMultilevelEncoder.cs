2a47bbe91264edda192fa2c8bda65fcdadb8c5b9
dmitriy.litichevskiy <litichevskiydv@gmail.com>
2021-02-25 10:09:06 +0300

    Naive encoding was implemented in linear multilevel encoder

---
+++ src/WaveletCodesTools/Encoding/LinearMultilevelEncoder.cs
@@ -0,0 +1,42 @@
+﻿namespace AppliedAlgebra.WaveletCodesTools.Encoding
+{
+    using System;
+    using GfAlgorithms.Matrices;
+    using GfPolynoms;
+    using MultilevelEncoderDependencies.GeneratingMatrixProvider;
+
+    /// <summary>
+    /// Wavelet codes linear encoder that supports multilevel wavelet transform
+    /// </summary>
+    public class LinearMultilevelEncoder : IMultilevelEncoder
+    {
+        private readonly FieldElementsMatrix _generatingMatrix;
+
+        /// <summary>
+        /// Initializes encoder dependencies
+        /// </summary>
+        /// <param name="generatingMatrixProvider">Generating matrix provider</param>
+        public LinearMultilevelEncoder(IGeneratingMatrixProvider generatingMatrixProvider)
+        {
+            if (generatingMatrixProvider == null)
+                throw new ArgumentNullException(nameof(generatingMatrixProvider));
+
+            _generatingMatrix = generatingMatrixProvider.GetGeneratingMatrix();
+        }
+
+        /// <inheritdoc/>
+        public FieldElement[] Encode(int codewordLength, FieldElement[] informationWord, MultilevelEncoderOptions options = null)
+        {
+            if (codewordLength <= 0)
+                throw new ArgumentException($"{nameof(codewordLength)} must be positive");
+            if (informationWord == null)
+                throw new ArgumentNullException(nameof(informationWord));
+            if (codewordLength < informationWord.Length)
+                throw new ArgumentException($"{nameof(codewordLength)} is too short");
+            if(informationWord.Length > _generatingMatrix.ColumnsCount)
+                throw new ArgumentException($"{nameof(informationWord)} is too long");
+
+            return (_generatingMatrix * FieldElementsMatrix.ColumnVector(informationWord)).GetColumn(0);
+        }
+    }
+}
---
+++ test/WaveletCodesTools.Tests/Encoding/LinearMultilevelEncoderTests.cs
@@ -0,0 +1,41 @@
+﻿namespace AppliedAlgebra.WaveletCodesTools.Tests.Encoding
+{
+    using GfAlgorithms.Matrices;
+    using GfAlgorithms.WaveletTransform.IterationFiltersCalculator;
+    using GfPolynoms.Extensions;
+    using GfPolynoms.GaloisFields;
+    using WaveletCodesTools.Encoding;
+    using WaveletCodesTools.Encoding.MultilevelEncoderDependencies.GeneratingMatrixProvider;
+    using WaveletCodesTools.Encoding.MultilevelEncoderDependencies.LevelMatricesProvider;
+    using Xunit;
+
+    public class LinearMultilevelEncoderTests
+    {
+        [Fact]
+        public void MustPerformEncodingForNaiveSchema()
+        {
+            // Given
+            var gf3 = GaloisField.Create(3);
+            var generatingMatrixProvider = new NaiveProvider(
+                new RecursionBasedProvider(
+                    new ConvolutionBasedCalculator(),
+                    2,
+                    (
+                        gf3.CreateElementsVector(1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0),
+                        gf3.CreateElementsVector(0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+                    )
+                ),
+                FieldElementsMatrix.CirculantMatrix(gf3, 0, 0, 0, 0, 0, 1),
+                FieldElementsMatrix.CirculantMatrix(gf3, 0, 0, 1)
+            );
+            var encoder = new LinearMultilevelEncoder(generatingMatrixProvider);
+
+            // When
+            var actualCodeword = encoder.Encode(12, gf3.CreateElementsVector(1, 1, 1));
+
+            // Then
+            var expectedCodeword = gf3.CreateElementsVector(1, 1, 2, 2, 1, 1, 2, 2, 1, 1, 2, 2);
+            Assert.Equal(expectedCodeword, actualCodeword);
+        }
+    }
+}
