64091509162c13f950263f32254cdd72e3699c3b
dmitriy.litichevskiy <litichevskiydv@gmail.com>
2021-03-02 22:48:33 +0300

    Codeword mutator contract was added

---
+++ src/CodesAbstractions/ICodewordMutator.cs
@@ -0,0 +1,18 @@
+﻿namespace AppliedAlgebra.CodesAbstractions
+{
+    using GfPolynoms;
+
+    /// <summary>
+    /// Codeword mutator contract
+    /// </summary>
+    public interface ICodewordMutator
+    {
+        /// <summary>
+        /// Modifies codeword in to achieve required length
+        /// </summary>
+        /// <param name="originalCodeword">Original codeword</param>
+        /// <param name="requiredLength">Required length</param>
+        /// <returns>New codeword</returns>
+        FieldElement[] Mutate(FieldElement[] originalCodeword, int requiredLength);
+    }
+}
