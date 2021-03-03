258a3050c0b192f174fa4a21b23237949777f99e
dmitriy.litichevskiy <litichevskiydv@gmail.com>
2021-03-03 09:17:14 +0300

    Basic codeword mutator was implemented

---
+++ src/CodesAbstractions/BasicCodewordMutator.cs
@@ -0,0 +1,32 @@
+﻿namespace AppliedAlgebra.CodesAbstractions
+{
+    using System;
+    using System.Linq;
+    using GfAlgorithms.Extensions;
+    using GfPolynoms;
+    using GfPolynoms.Extensions;
+
+
+    public class BasicCodewordMutator : ICodewordMutator
+    {
+        public FieldElement[] Mutate(FieldElement[] originalCodeword, int requiredLength)
+        {
+            if (originalCodeword == null)
+                throw new ArgumentNullException(nameof(originalCodeword));
+            if (originalCodeword.Length == 0)
+                throw new ArgumentException($"{nameof(originalCodeword)} must not be empty");
+            if (requiredLength <= 0)
+                throw new ArgumentException($"{requiredLength} must be positive");
+
+            var field = originalCodeword.GetField();
+            if (originalCodeword.Length + 1 == requiredLength)
+                return originalCodeword.Concat(new[] {originalCodeword.Aggregate(field.Zero(), (checkSum, x) => checkSum + x)}).ToArray();
+            if (originalCodeword.Length == requiredLength)
+                return originalCodeword;
+            if (originalCodeword.Length > requiredLength)
+                return originalCodeword.Take(requiredLength).ToArray();
+
+            throw new InvalidOperationException("Codeword length can't be adjusted to requirements");
+        }
+    }
+}
---
+++ test/CodesAbstractions.Tests/BasicCodewordMutatorTests.cs
@@ -0,0 +1,102 @@
+﻿namespace AppliedAlgebra.CodesAbstractions.Tests
+{
+    using System;
+    using GfPolynoms;
+    using GfPolynoms.Extensions;
+    using GfPolynoms.GaloisFields;
+    using JetBrains.Annotations;
+    using Xunit;
+
+    public class BasicCodewordMutatorTests
+    {
+        public class MutateParametersValidationTestCase
+        {
+            public FieldElement[] OriginalCodeword { get; set; }
+
+            public int RequiredLength { get; set; }
+        }
+
+        public class CodewordMutationTestCase : MutateParametersValidationTestCase
+        {
+            public FieldElement[] Expected { get; set; }
+        }
+
+        private readonly BasicCodewordMutator _codewordMutator;
+
+        [UsedImplicitly]
+        public static TheoryData<MutateParametersValidationTestCase> MutateParametersValidationTestCases;
+        [UsedImplicitly]
+        public static TheoryData<CodewordMutationTestCase> CodewordMutationTestCases;
+
+        static BasicCodewordMutatorTests()
+        {
+            var gf3 = GaloisField.Create(3);
+            var originalCodeword = gf3.CreateElementsVector(0, 1, 2, 1);
+
+            MutateParametersValidationTestCases
+                = new TheoryData<MutateParametersValidationTestCase>
+                  {
+                      new MutateParametersValidationTestCase(),
+                      new MutateParametersValidationTestCase {OriginalCodeword = new FieldElement[0]},
+                      new MutateParametersValidationTestCase{OriginalCodeword = originalCodeword, RequiredLength = -1}
+                  };
+            CodewordMutationTestCases
+                = new TheoryData<CodewordMutationTestCase>
+                  {
+                      new CodewordMutationTestCase
+                      {
+                          OriginalCodeword = originalCodeword,
+                          RequiredLength = originalCodeword.Length + 1,
+                          Expected = gf3.CreateElementsVector(0, 1, 2, 1, 1)
+                      },
+                      new CodewordMutationTestCase
+                      {
+                          OriginalCodeword = originalCodeword,
+                          RequiredLength = originalCodeword.Length,
+                          Expected = originalCodeword
+                      },
+                      new CodewordMutationTestCase
+                      {
+                          OriginalCodeword = originalCodeword,
+                          RequiredLength = originalCodeword.Length -1,
+                          Expected = gf3.CreateElementsVector(0, 1, 2)
+                      }
+                  };
+        }
+
+        public BasicCodewordMutatorTests()
+        {
+            _codewordMutator = new BasicCodewordMutator();
+        }
+
+        [Theory]
+        [MemberData(nameof(MutateParametersValidationTestCases))]
+        public void MutateMustValidateParameters(MutateParametersValidationTestCase testCase)
+        {
+            Assert.ThrowsAny<ArgumentException>(() => _codewordMutator.Mutate(testCase.OriginalCodeword, testCase.RequiredLength));
+        }
+
+        [Theory]
+        [MemberData(nameof(CodewordMutationTestCases))]
+        public void MustMutateCodeword(CodewordMutationTestCase testCase)
+        {
+            // When
+            var actual = _codewordMutator.Mutate(testCase.OriginalCodeword, testCase.RequiredLength);
+
+            // Then
+            Assert.Equal(testCase.Expected, actual);
+        }
+
+        [Fact]
+        public void CanNotAchieveRequiredLength()
+        {
+            // Given
+            var gf3 = GaloisField.Create(3);
+            var originalCodeWord = gf3.CreateElementsVector(1, 2, 1, 0);
+            var requiredLength = originalCodeWord.Length + 2;
+
+            // When, Then
+            Assert.Throws<InvalidOperationException>(() => _codewordMutator.Mutate(originalCodeWord, requiredLength));
+        }
+    }
+}
