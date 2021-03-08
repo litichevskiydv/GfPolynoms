8470966ebedf4894edf8a6d0eb71b1514d02f7ea
dmitriy.litichevskiy <litichevskiydv@gmail.com>
2021-03-08 23:13:15 +0300

    Processor usage was optimized

--- src/CodesResearchTools/Analyzers/CodeDistance/LinearCodeDistanceAnalyzer.cs
+++ src/CodesResearchTools/Analyzers/CodeDistance/LinearCodeDistanceAnalyzer.cs
@@ -12,6 +12,28 @@
 
     public class LinearCodeDistanceAnalyzer : CodeDistanceAnalyzerBase
     {
+        private int ProcessInformationWord(
+            FieldElement[] informationWord,
+            Func<FieldElement[], FieldElement[]> encodingProcedure,
+            CodeDistanceAnalyzerOptions options,
+            ParallelLoopState loopState,
+            int localCodeDistance,
+            ref long processedCodewords
+        )
+        {
+            if (Interlocked.Increment(ref processedCodewords) % options.LoggingResolution == 0)
+                Logger.LogInformation("Processed {processedCodewords} pairs, code distance {codeDistance}", processedCodewords, localCodeDistance);
+
+            var distance = encodingProcedure(informationWord).Count(x => x.Representation != 0);
+            if (distance == 0 && informationWord.All(x => x.Representation == 0))
+                return localCodeDistance;
+
+            if (options.CodeDistanceMinimumThreshold.HasValue && distance < options.CodeDistanceMinimumThreshold.Value)
+                loopState.Stop();
+
+            return Math.Min(localCodeDistance, distance);
+        }
+
         protected override int AnalyzeInternal(
             GaloisField field,
             int informationWordLength,
@@ -19,26 +41,49 @@
             CodeDistanceAnalyzerOptions options
         )
         {
+            var firstPortionLength = Math.Min(
+                Math.Max((int)Math.Ceiling(Math.Log(options.MaxDegreeOfParallelism, field.Order)), 1),
+                informationWordLength
+            );
+            var secondPortionLength = informationWordLength - firstPortionLength;
+
             var processedCodewords = 0L;
             var codeDistance = int.MaxValue;
             Parallel.ForEach(
-                GenerateMappings(field, informationWordLength, encodingProcedure).Skip(1),
+                VariantsIterator.IterateVectors(field, firstPortionLength),
                 new ParallelOptions {MaxDegreeOfParallelism = options.MaxDegreeOfParallelism},
                 () => int.MaxValue,
-                (mapping, loopState, localCodeDistance) =>
+                (informationWordFirstPart, loopState, localCodeDistance) =>
                 {
                     if (loopState.IsStopped)
                         return localCodeDistance;
 
-                    if (Interlocked.Increment(ref processedCodewords) % options.LoggingResolution == 0)
-                        Logger.LogInformation("Processed {processedCodewords} pairs, code distance {codeDistance}", processedCodewords, localCodeDistance);
+                    if (secondPortionLength == 0)
+                        return ProcessInformationWord(
+                            informationWordFirstPart,
+                            encodingProcedure,
+                            options,
+                            loopState,
+                            localCodeDistance,
+                            ref processedCodewords
+                        );
 
-                    var distance = mapping.codeword.Count(x => x.Representation != 0);
-                    if (options.CodeDistanceMinimumThreshold.HasValue && distance < options.CodeDistanceMinimumThreshold.Value)
-                        loopState.Stop();
+                    foreach (var informationWordSecondPart in VariantsIterator.IterateVectors(field, secondPortionLength))
+                    {
+                        if (loopState.IsStopped)
+                            return localCodeDistance;
 
-                    return Math.Min(localCodeDistance, distance);
+                        localCodeDistance = ProcessInformationWord(
+                            informationWordFirstPart.Concat(informationWordSecondPart).ToArray(),
+                            encodingProcedure,
+                            options,
+                            loopState,
+                            localCodeDistance,
+                            ref processedCodewords
+                        );
+                    }
 
+                    return localCodeDistance;
                 },
                 localCodeDistance => { InterlockedExtensions.Min(ref codeDistance, localCodeDistance); }
             );
